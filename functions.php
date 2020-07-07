d9f8a790860c4efd555a654bdcd3a810f7098da7
Nathan Rollins <nathan.rollins@asu.edu>
2020-07-06 17:52:55 -0700

    Remove woocommerce references

--- functions.php
+++ functions.php
@@ -21,7 +21,6 @@ $understrap_includes = array(
 	'/custom-comments.php',                 // Custom Comments file.
 	'/jetpack.php',                         // Load Jetpack compatibility file.
 	'/class-wp-bootstrap-navwalker.php',    // Load custom WordPress nav walker. Trying to get deeper navigation? Check out: https://github.com/understrap/understrap/issues/567.
-	'/woocommerce.php',                     // Load WooCommerce functions.
 	'/editor.php',                          // Load Editor functions.
 	'/deprecated.php',                      // Load deprecated functions.
 );
