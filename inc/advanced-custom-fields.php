a120e42b28a4ec5b315a79449d05de3442cd6cda
Nathan Rollins <nathan.rollins@asu.edu>
2020-07-07 13:02:47 -0700

    Install and configure Advanced Custom Fields in Theme

---
+++ inc/advanced-custom-fields.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Advanced Custom Fields Integration into Theme
+ *
+ * @package asu-web-standards-2020
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+// Define path and URL to the integrated ACF plugin in the theme.
+define( 'ACF_PLUGIN_PATH', get_stylesheet_directory() . '/inc/acf/' );
+define( 'ACF_PLUGIN_URL', get_stylesheet_directory_uri() . '/inc/acf/' );
+
+// Include the ACF plugin.
+include_once( ACF_PLUGIN_PATH . 'acf.php' );
+
+// Customize the url setting to fix incorrect asset URLs.
+function asu_wp2020_acf_settings_url( $url ) {
+    return ACF_PLUGIN_URL;
+}
+add_filter('acf/settings/url', 'asu_wp2020_acf_settings_url');
+
+// Show the ACF admin menu?
+function asu_wp2020_acf_settings_show_admin( $show_admin ) {
+    return true;
+}
+add_filter('acf/settings/show_admin', 'asu_wp2020_acf_settings_show_admin');
