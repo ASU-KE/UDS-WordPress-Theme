3785b9e0c0f97e0ace0907e2356027aaffc31c35
Nathan Rollins <nathan.rollins@asu.edu>
2020-07-07 00:51:38 -0700

    Update enqueued script ids

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -18,12 +18,12 @@ if ( ! function_exists( 'asu_wp2020_scripts' ) ) {
 		$theme_version = $the_theme->get( 'Version' );
 
 		$css_version = $theme_version . '.' . filemtime( get_template_directory() . '/css/theme.min.css' );
-		wp_enqueue_style( 'understrap-styles', get_template_directory_uri() . '/css/theme.min.css', array(), $css_version );
+		wp_enqueue_style( 'asu-wp2020-styles', get_template_directory_uri() . '/css/theme.min.css', array(), $css_version );
 
 		wp_enqueue_script( 'jquery' );
 
 		$js_version = $theme_version . '.' . filemtime( get_template_directory() . '/js/theme.min.js' );
-		wp_enqueue_script( 'understrap-scripts', get_template_directory_uri() . '/js/theme.min.js', array(), $js_version, true );
+		wp_enqueue_script( 'asu-wp2020-scripts', get_template_directory_uri() . '/js/theme.min.js', array(), $js_version, true );
 
 		$fa_js_version = $theme_version . '.' . filemtime( get_template_directory() . '/js/fontawesome/all.min.js' );
 		wp_enqueue_script( 'asu-wp2020-fa-scripts', get_template_directory_uri() . '/js/fontawesome/all.min.js', array(), $fa_js_version, true );
--- inc/wpcom.php
+++ inc/wpcom.php
@@ -44,6 +44,6 @@ if ( ! function_exists( 'asu_wp2020_wpcom_styles' ) ) {
 	 * WordPress.com-specific styles
 	 */
 	function asu_wp2020_wpcom_styles() {
-		wp_enqueue_style( 'understrap-wpcom', get_template_directory_uri() . '/inc/style-wpcom.css', array(), '20160411' );
+		wp_enqueue_style( 'asu-wp2020-wpcom', get_template_directory_uri() . '/inc/style-wpcom.css', array(), '20160411' );
 	}
 }
