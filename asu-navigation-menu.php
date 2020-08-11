9af3709c36e864c748c03e63758d7fbfa1b9dba0
Walter McConnell <Walter.McConnell@asu.edu>
2020-08-11 09:27:43 -0700

    Refactor Nav Menu and Fix Single-Column Issue
    Updated the nav menu code to pull some common code into functions, and to fix the fact that my original "fix" did not work for single-column drop-downs:
    
    * Added the method `get_menu_depth()` that returns the depth of the menu for use in other code. I stuck with strings that were already being used, so each item is given a depth of "single", "children" or "grandchildren"
    
    * Moved the common code for rendering actual menu links into its own function. It allowed for a bit of cleanup in the main loop that made things more readable.
    
    * Moved code for rendering the actual menu bar link itself to a new function. This function uses the new depth value to determine how to render the link (as either a simple <a> tag, or a drop-down header link.
    
    It's still a work in progress, as there is room for improvement here.

--- asu-navigation-menu.php
+++ asu-navigation-menu.php
@@ -15,53 +15,157 @@ $menu_name = 'primary';
 $menu_items = asu_wp2020_get_menu_formatted_array($menu_name);
 // vd($menu_items);
 
+
+/**
+ * Get the maximum depth of this particular menu item by inspecting
+ * the 'children' sub-array at each level to determine whether or not
+ * it is empty
+ *
+ * @param Array $item array of top-level menu items
+ * @return String Maximum depth of the menu: single, children, or grandchildren
+ */
+function get_menu_depth( $item = null ) {
+	if( empty( $item ) ) {
+		wp_die( 'Cannot find depth of a menu item that was not provided, or is empty.' );
+	}
+
+	// presume that we do not have any children or grandchildren
+	$depth = 'single';
+
+	if( !empty( $item['children'] ) ) {
+		// we have at least children, since the array is not empty
+		$depth = 'children';
+
+		// check for any grandchildren, exiting if we find any
+		foreach( $item['children'] as $child ) {
+			if( !empty( $child['children'] ) ) {
+				$depth = 'grandchildren';
+				break;
+			}
+		}
+	}
+
+	return $depth;
+}
+
+/**
+ * Renders the individual links from the provided child/grandchild list
+ *
+ * @param Array $children The array of links for one column
+ * @return String $links A string containing all the <a> tags for the column
+ */
+function render_column_links ( $children = array() ) {
+
+	if( empty( $children ) ) {
+		return 'No Menu Links';
+	}
+
+	$links = "";
+
+	foreach ($children as $child) {
+		$link = '<a class="dropdown-item" href="%1$s" title="%2$s">%2$s</a>';
+		$links .= wp_kses(sprintf($link, $child['url'], $child['title']), wp_kses_allowed_html('post'));
+	}
+
+	return $links;
+}
+
+/**
+ * Renders the top-level link, either as a normal nav link, or a drop-down link
+ * Note that we're using 'default' to render our actual default, and not testing
+ * for the 'single' case
+ *
+ * @param Array $item the navigation item whose link we want to render
+ * @return String $link the rendered navigation link
+ */
+function render_nav_item_link( $item ) {
+	$link = "";
+
+	switch( $item['depth'] ) {
+
+		case 'children':
+		case 'grandchildren':
+			$template = '<a class="nav-link" href="%1$s" id="dropdown-one-col" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+			%2$s
+			<span class="fa fa-chevron-down"></span>
+			</a>';
+			$link = wp_kses(sprintf($template, $item['url'], $item['title']), wp_kses_allowed_html('post'));
+			return $link;
+		break;
+
+		default:
+			$template = '<a class="nav-link" href="%1$s" title="%2$s">%2$s</a>';
+			$link = wp_kses(sprintf($template, $item['url'], $item['title']), wp_kses_allowed_html('post'));
+			return $link;
+	}
+}
+
+
+/**
+ * Main navigation loop
+ */
 foreach ($menu_items as $item) :
-	// vd($item);
+	// evaluate item for: single, children (one column), or grandchildren (multiple columns)
+	$item['depth'] = get_menu_depth( $item );
 
-	// if no children, basic menu link
-	if (empty($item['menu_item_parent']) && empty($item['children'])) :
-		$link = '<a class="nav-link" href="%1$s" title="%2$s">%2$s</a>';
-		echo wp_kses(sprintf($link, $item['url'], $item['title']), wp_kses_allowed_html('post'));
-	endif; // basic menu link
 
-	// if menu item has children, we have a dropdown menu
-	if (!empty($item['children'])) : ?>
-		<div class="nav-item dropdown">
-			<a class="nav-link" href="#" id="dropdown-one-col" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-				<?= $item['title'] ?>
-				<span class="fa fa-chevron-down"></span>
-			</a>
-			<div class="dropdown-menu dropdown-columns" aria-labelledby="dropdown-one-col">
+	// render each item based on the depth
+	switch ($item['depth']) {
+		/*
+		 * Items with no children are rendered as basic menu item links
+		 */
+		case 'single':
+			echo render_nav_item_link( $item );
+			break;
+
+		/*
+		 * Items with children, but no grandchilren, are rendered as single-column
+		 * drop-downs with the parent name as the column heading and the children
+		 * as the menu items
+		 */
+		case 'children':
+			?>
+			<div class="nav-item dropdown">
+				<?php echo render_nav_item_link( $item ); ?>
+				<div class="dropdown-menu dropdown-columns" aria-labelledby="dropdown-one-col">
+					<div class="dropdown-col">
+						<?php echo render_column_links( $item['children'] ); ?>
+					</div>
+				</div>
+			</div>
+			<?php
+			break;
+
+		/*
+		 * Items with grandchildren are rendered as multi-column drop-downs,
+		 * with the child-level title as the column heading, and the grandchildren as
+		 * the menu items.
+		 */
+		case 'grandchildren':
+			?>
+			<div class="nav-item dropdown">
+				<?php echo render_nav_item_link( $item ); ?>
+				<div class="dropdown-menu dropdown-columns" aria-labelledby="dropdown-one-col">
 				<?php
-				// begin constructing the dropdown column
-				$column = '<div class="dropdown-col">';
 
+				// outer loop
 				foreach ($item['children'] as $child) :
-					// if menu item has no children, this is a basic one-column dropdown
-					if (empty($child['children'])) :
-						$link = '<a class="dropdown-item" href="%1$s" title="%2$s">%2$s</a>';
-						$column .= wp_kses(sprintf($link, $child['url'], $child['title']), wp_kses_allowed_html('post'));
+					$column = '<div class="dropdown-col">';
+					$column .= "<h3>" . wp_kses($child['title'], wp_kses_allowed_html('post')) . "</h3>";
+					$column .= render_column_links( $child['children'] ); // inner loop
+					$column .= '</div>';
 
-					// with children, we have multiple columns to render
-					else :
-						// because we need to render multiple columns inside the dropdown,
-						// we are intentionally resetting $column started above to generate this multi-column dropdown
-						$column = '<div class="dropdown-col">';
-						$column .= "<h3>" . wp_kses($child['title'], wp_kses_allowed_html('post')) . "</h3>";
-						foreach ($child['children'] as $grandchild) :
-							$link = '<a class="dropdown-item" href="%1$s" title="%2$s">%2$s</a>';
-							$column .= wp_kses(sprintf($link, $grandchild['url'], $grandchild['title']), wp_kses_allowed_html('post'));
-						endforeach;
-						$column .= '</div>';
-
-						// output this column
-						echo $column;
-					endif;
+					// output this column
+					echo $column;
 				endforeach;
 				?>
 			</div>
 		</div>
-<?php
-	endif;
+		<?php
+			break;
 
+		default:
+			# code...
+			break;
+	}
 endforeach;
