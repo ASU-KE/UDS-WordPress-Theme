d1bfe67c9f2f08e257e6fe893c78dbbe7279ce31
Nathan Rollins <nathan.rollins@asu.edu>
2020-10-17 13:25:28 -0700

    Fix invalid package references

--- 404.php
+++ 404.php
@@ -2,7 +2,7 @@
 /**
  * The template for displaying 404 pages (not found)
  *
- * @package uds-wordpress-theme-theme
+ * @package uds-wordpress-theme
  */
 
 // Exit if accessed directly.
--- inc/extras.php
+++ inc/extras.php
@@ -4,7 +4,7 @@
  *
  * Eventually, some of the functionality here could be replaced by core features.
  *
- * @package uds-wordpress-theme-theme
+ * @package uds-wordpress-theme
  */
 
 // Exit if accessed directly.
