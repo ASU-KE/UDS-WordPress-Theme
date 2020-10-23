18da4b2d8fc3d1e3aab25ad59ac4937669211638
Zainab Alsidiki <zalsidik@asu.edu>
2020-10-23 16:45:59 -0700

    fixed CS error in JS

--- js/metaboxes.js
+++ js/metaboxes.js
@@ -1,31 +1,21 @@
-(function($) {
-    $(document).ready(function (){
-
-    		/* Image opts selection */
-            $('body').on('click', 'img.img-select', function(e){
+(function( $ ) {
+    $( document ).ready(function() {
+            $( 'body' ).on( 'click', 'img.img-select', function( e ) {
+							var v = $( this ).closest( 'li' ).find( 'input' ).val();
                 e.preventDefault();
-                $(this).closest('ul').find('img.img-select').removeClass('selected');
-                $(this).addClass('selected');
-              //  $(this).closest('ul').find('input').removeAttr('checked');
-                $(this).closest('li').find('input').prop( "checked", true );
+                $( this ).closest( 'ul' ).find( 'img.img-select' ).removeClass( 'selected' );
+                $( this ).addClass( 'selected' );
+                $( this ).closest( 'li' ).find( 'input' ).prop( 'checked', true );
 
-                if($(this).closest('ul').hasClass('next-hide')){
-
-                    var v = $(this).closest('li').find('input').val();
-                    if(v == 'none' || v == 0 || v == 'fixed'){
-                         $(this).closest('ul').parent().find('select').fadeOut(400);
+                if ( $( this ).closest( 'ul' ).hasClass( 'next-hide' ) ) {
+                    if ( 'none' == v || 0 == v || 'fixed' == v ) {
+                         $( this ).closest( 'ul' ).parent().find( 'select' ).fadeOut( 400 );
                     } else {
-                        $(this).closest('ul').parent().find('select').fadeIn(400);
+                        $( this ).closest( 'ul' ).parent().find( 'select' ).fadeIn( 400 );
                     }
                 }
             });
 
-
-
-
-
     });
 
-
-
-})(jQuery);
+})( jQuery );
