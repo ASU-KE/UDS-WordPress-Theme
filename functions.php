ec17f151b49274f6ca7fd13ad1e729daaae2e4e5
Walter McConnell <Walter.McConnell@asu.edu>
2020-11-29 22:38:08 -0700

    Load Context Help
    * Updated 'functions.php' to load our new context help.
    
    * fixed a typo

--- functions.php
+++ functions.php
@@ -17,6 +17,7 @@ $uds_wp_includes = array(
 	'/wpautop.php',                         // Disable wpautop.
 	'/widgets.php',                         // Register widget area.
 	'/enqueue.php',                         // Enqueue scripts and styles.
+	'/uds-contextual-help.php',                 // theme-specific context-senstive help tabs
 	'/template-tags.php',                   // Custom template tags for this theme.
 	'/pagination.php',                      // Custom pagination for this theme.
 	'/hooks.php',                           // Custom hooks.
@@ -26,7 +27,7 @@ $uds_wp_includes = array(
 	'/jetpack.php',                         // Load Jetpack compatibility file.
 	'/wp-custom-fields.php',                // Disable WP Core custom fields metaboxes.
 	'/tgm-plugin-activation.php',           // Load TGM Plugin Activation script for dependent plugin recommendations.
-	'/class-wp-social-media-walker.php',    // Load custom WordPress nav walker for footer Social Media menu wdiget.
+	'/class-wp-social-media-walker.php',    // Load custom WordPress nav walker for footer Social Media menu widget.
 	'/wp-custom-menu.php',                  // Load custom menu builder functions.
 	'/editor.php',                          // Load Editor functions.
 	'/deprecated.php',                      // Load deprecated functions.
