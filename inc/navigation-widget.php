53c46c27f88b38270b81e8b282d0727843b179c0
Zainab Alsidiki <zalsidik@asu.edu>
2020-12-04 15:28:06 -0700

    Added current_page_item to classes to be replaced with .active class

--- inc/navigation-widget.php
+++ inc/navigation-widget.php
@@ -36,6 +36,9 @@ function uds_wp_theme_special_nav_class( $classes, $item ) {
 	if ( in_array( 'current-menu-item', $classes ) ) {
 		$classes = array_diff( $classes, array( 'current-menu-item', 'active' ) );
 		$classes[] = 'active ';
+	} else if ( in_array( 'current_page_item', $classes ) ) {
+		$classes = array_diff( $classes, array( 'current_page_item', 'active' ) );
+		$classes[] = 'active ';
 	}
 	return $classes;
 }
