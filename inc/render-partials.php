4e5849539b788e74b6f27a0bb7b152ea3210a869
Walter McConnell <Walter.McConnell@asu.edu>
2020-12-30 14:44:51 -0700

    Remove Leftover Comment Line
    Removed a unnecessary comment line that was overlooked during the first round of cleanup.

--- inc/render-partials.php
+++ inc/render-partials.php
@@ -145,7 +145,6 @@ function uds_wp_render_footer_branding_row() {
  * renders a link with that value as the URL.
  */
 function uds_wp_render_contact_link() {
-	// =============================
 	$contact_url = get_theme_mod( 'contact_url' );
 	$contact_template = '<p class="contact-link"><a href="%1$s">Contact Us</a></p>';
 
