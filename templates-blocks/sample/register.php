f66a623b5eb195cf55ec16cf1ddc1e29cab51885
Walter McConnell <Walter.McConnell@asu.edu>
2021-01-19 14:04:24 -0700

    Add new settings to the sample block.
    * Added keywords
    
    * Added 'mode' setting to make the block always appear in Edit mode when placed on the page
    
    * Added support for a preview of the block when inserting on the page

--- templates-blocks/sample/register.php
+++ templates-blocks/sample/register.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Block Registration
  *
@@ -7,19 +6,43 @@
  * Author: Walt
  * Version: 1.0
  *
+ * @package UDS WordPress Theme
+ *
  * Notes: A simple block, for demonstration purposes, that creates a stylized message
- * across the entire content area. This block is attempting to use as many settings
- * from 'acf_register_block_type' as it can, so that there are examples of how to use
- * them. You may end up removing some settings that are not needed.
+ * across the entire content area. This block is attempting to use the settings we are
+ * most likely to want, but you can find all of the available settings in the ACF
+ * documentation here:
+ * https://www.advancedcustomfields.com/resources/acf_register_block_type/
+ *
+ * Note: for the icon, you can use any Dashicon:
+ * https://developer.wordpress.org/resource/dashicons, OR an actual <SVG></SVG> tag
+ * with all the required content. In this example, I'm using a dashicon.
  */
-acf_register_block_type( array(
-	'name'              => 'sample', // internal name, like a slug.
-	'title'             => __('Sample Block'), // name the user will see.
-	'description'       => __('A sample block for learning ACF Pro blocks.'), // description the user will see.
-	'icon'              => '',// Dashicon, or custom SVG code, for the icon
-	'render_template'   => 'templates-blocks/sample/sample.php', // location of the block's template.
-	'category'          => 'UDS', // category this block appears in.
-	'supports'          => array(
-		'align' => false, // Remove the align button in the editor toolbar
+
+acf_register_block_type(
+	array(
+		'name'              => 'sample', // internal name, like a slug.
+		'title'             => __( 'Sample Block', 'uds-wordpress-theme' ), // name the user will see.
+		'description'       => __( 'A sample block for learning ACF Pro blocks.', 'uds-wordpress-theme' ), // description the user will see.
+		'icon'              => 'format-quote', // Dashicon, or custom SVG code, for the icon.
+		'render_template'   => 'templates-blocks/sample/sample.php', // location of the block's template.
+		'category'          => 'layout', // category this block appears in.
+		'keywords'          => array( 'callout', 'quote', 'text' ),
+		'supports'          => array(
+			'align' => false, // Remove the align button in the editor toolbar.
+		),
+		'mode'              => 'edit', // make this block default to full edit mode when added to the page.
+		'example'  => array(
+			'attributes' => array(
+				'mode' => 'preview',
+				'data' => array(
+					'title'                   => 'A Sample Block',
+					'background_style'        => 'network-white',
+					'container_type'          => 'container-fluid',
+					'add_inner_container'     => true,
+					'content'                 => 'Lorem ipsum sit dolor amet.',
+				),
+			),
+		),
 	)
-));
+);
