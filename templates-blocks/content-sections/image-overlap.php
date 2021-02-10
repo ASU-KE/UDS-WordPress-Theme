0e3690769e2e86854973abb35b269d36f5e99e15
Steve Ryan <steve.ryan@asu.edu>
2021-02-10 13:45:44 -0700

    Code linting.

--- templates-blocks/content-sections/image-overlap.php
+++ templates-blocks/content-sections/image-overlap.php
@@ -3,30 +3,35 @@
  * UDS Content Sections: Content Image Overlap
  *
  * @package UDS WordPress Theme
- *
  */
 
-$background = get_field('uds_image_overlap_background');
-$orientation = get_field('uds_image_overlap_orientation');
+$background = get_field( 'uds_image_overlap_background' );
+$orientation = get_field( 'uds_image_overlap_orientation' );
 
 // Allowed blocks inside inner content.
-$allowed_blocks = array( 'core/heading', 'core/paragraph', 'core/separator', 'core/html', 'core/list');
+$allowed_blocks = array( 'core/heading', 'core/paragraph', 'core/separator', 'core/html', 'core/list' );
 
 // Pre-populate the InnerBlocks area with some content.
 $template = array(
-	array('core/heading', array(
-		'level' => 3,
-		'content' => 'Title Goes Here',
-	)),
-    array( 'core/paragraph', array(
-        'content' => 'Creating daily standups with the possibility to make the logo bigger. Execute analytics yet take this offline. Repurpose below the line with the possibility to infiltrate new markets. Growing first party data and try to create synergy.',
-	)),
+	array(
+		'core/heading',
+		array(
+			'level' => 3,
+			'content' => 'Title Goes Here',
+		),
+	),
+	array(
+		'core/paragraph',
+		array(
+			'content' => 'Creating daily standups with the possibility to make the logo bigger. Execute analytics yet take this offline. Repurpose below the line with the possibility to infiltrate new markets. Growing first party data and try to create synergy.',
+		),
+	),
 );
 
 // Echo the block.
 if ( $background ) {
 
-	if ( "left" === $orientation ) {
+	if ( 'left' === $orientation ) {
 		echo '<div class="uds-image-overlap content-left">';
 	} else {
 		echo '<div class="uds-image-overlap">';
