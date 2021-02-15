37833c59c2db4f69c4a549e9118ec2832ac9bea3
Walter McConnell <Walter.McConnell@asu.edu>
2021-02-15 14:58:00 -0700

    Update Hero Template for New Color Options and Fix Size Bug
    Updated the hero template to support the new options for white text, and a white highlighting. Also fixed an issue where the class names that determine the size of the hero were not being set properly due to a field name change.

--- templates-global/hero.php
+++ templates-global/hero.php
@@ -11,8 +11,10 @@ defined( 'ABSPATH' ) || exit;
 
 $hero_asset_url = get_field( 'hero_asset_url' );
 $hero_type = get_field( 'hero_type' );
+$hero_size = get_field( 'hero_size' );
 $hero_title = get_field( 'hero_title' );
 $hero_highlight = get_field( 'hero_highlight' );
+$title_color = get_field( 'title_color' );
 $hero_text = get_field( 'hero_text' );
 $hero_call_to_action_url = get_field( 'hero_call_to_action_url' );
 $hero_call_to_action_text = get_field( 'hero_call_to_action_text' );
@@ -22,17 +24,40 @@ if ( empty( $hero_highlight ) || 'none' == $hero_highlight ) {
 	$hero_highlight = '';
 }
 
+/**
+ * The text color of the hero title is usually black. However, when 'none' is
+ * chosen, we allow the user to choose between black or white text to best fit
+ * with the image.
+ */
+if ('white' == $title_color && 'none' == $hero_highlight || empty( $hero_highlight ) ) {
+	$title_color_class = "text-white";
+}else{
+	$title_color_class = '';
+}
+
+switch ($hero_size) {
+	case 'small':
+		$hero_size_class = 'uds-hero-sm';
+		break;
+	case 'medium':
+		$hero_size_class = 'uds-hero-md';
+	case 'large':
+		$hero_size_class = 'uds-hero-lg';
+	default:
+		$hero_size_class = 'uds-hero-md';
+		break;
+}
 
 // Check for a hero bg image and if present build the hero. Otherwise show the title of the page.
 if ( ! empty( $hero_asset_url ) ) :
 	?>
-<div class="uds-hero <?php echo $hero_type; ?>" style="background-image: linear-gradient(180deg, #19191900 0%, #191919c9 100%), url('<?php echo wp_kses( $hero_asset_url, wp_kses_allowed_html( 'strip' ) ); ?>')">
-	<div class="container uds-hero-container">
+<div class="uds-hero <?php echo $hero_size_class; ?>" style="background-image: linear-gradient(180deg, #19191900 0%, #191919c9 100%), url('<?php echo wp_kses( $hero_asset_url, wp_kses_allowed_html( 'strip' ) ); ?>')">
+	<div class="container uds-hero-container lazyloaded">
 		<?php
 		if ( ! empty( $hero_title ) ) :
 			?>
 		<h1 class="col-md-8">
-			<span class="<?php echo $hero_highlight; ?>"><?php echo wp_kses( $hero_title, wp_kses_allowed_html( 'strip' ) ); ?></span>
+			<span class="<?php echo $hero_highlight; ?> <?php echo $title_color_class; ?>"><?php echo wp_kses( $hero_title, wp_kses_allowed_html( 'strip' ) ); ?></span>
 		</h1>
 			<?php
 		endif;
