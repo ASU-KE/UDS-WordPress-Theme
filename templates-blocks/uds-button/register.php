0ee596784136442d88f44010d0bf7a4a96bdcc56
Justin <justin.holloway@asu.edu>
2021-02-17 13:57:36 -0700

    Comments and coding standards

--- templates-blocks/uds-button/register.php
+++ templates-blocks/uds-button/register.php
@@ -7,16 +7,6 @@
  * Version: 1.0
  *
  * @package UDS WordPress Theme
- *
- * Notes: A simple block, for demonstration purposes, that creates a stylized message
- * across the entire content area. This block is attempting to use the settings we are
- * most likely to want, but you can find all of the available settings in the ACF
- * documentation here:
- * https://www.advancedcustomfields.com/resources/acf_register_block_type/
- *
- * Note: for the icon, you can use any Dashicon:
- * https://developer.wordpress.org/resource/dashicons, OR an actual <SVG></SVG> tag
- * with all the required content. In this example, I'm using a dashicon.
  */
 
 acf_register_block_type(
--- templates-blocks/uds-button/uds-button.php
+++ templates-blocks/uds-button/uds-button.php
@@ -3,14 +3,11 @@
  * UDS Button Block
  *
  * @package UDS WordPress Theme
+ * @author Justin Holloway
  *
- * This is a button block for using within the UDS WordPress theme. This
- * particular block is a callout with a title and some text, and uses ACF functions
- * to bring in particular class names and content. This is an example of a larger
- * 'page section' kind of block, with all the Bootstrap containers, rows, and
- * columns as part of the markup. It would be intended for use only in our full
- * width page template, and NOT in the sidebar template.
+ * This is a button block for using within the UDS WordPress theme. This button allows for selecting size and color.
  */
+
 ?>
 
 <div class="uds-button">
@@ -22,33 +19,33 @@
 	$button_size = get_field( 'button_size' );
 	$new_tab = get_field( 'new_tab' );
 
-	if ($new_tab) {
-		$target = 'target="_blank"';
-	}else {
-		$target = '';
-	}
+if ( $new_tab ) {
+	$target = 'target="_blank"';
+} else {
+	$target = '';
+}
 
-	if ($external_link) {
-		$rel = 'rel="noopener noreferrer"';
-	}else {
-		$rel = '';
-	}
+if ( $external_link ) {
+	$rel = 'rel="noopener noreferrer"';
+} else {
+	$rel = '';
+}
 
-	if ( 'default' !== $button_size ) {
-		$button_size = 'btn-' . $button_size;
-	} else {
-		$button_size = '';
-	}
+if ( 'default' !== $button_size ) {
+	$button_size = 'btn-' . $button_size;
+} else {
+	$button_size = '';
+}
 
-	if ( get_field( 'icon' ) ) {
-		$button_icon = sanitize_text_field ( get_field( 'icon' ) );
-		$icon_span = '<span class="fas fa-' .  $button_icon . '"></span>&nbsp;&nbsp;';
-	} else {
-		$icon_span = '';
-	}
+if ( get_field( 'icon' ) ) {
+	$button_icon = sanitize_text_field( get_field( 'icon' ) );
+	$icon_span = '<span class="fas fa-' . $button_icon . '"></span>&nbsp;&nbsp;';
+} else {
+	$icon_span = '';
+}
 
 
-$button_icon = sanitize_text_field ( get_field( 'icon' ) );
+$button_icon = sanitize_text_field( get_field( 'icon' ) );
 
 
 ?>
