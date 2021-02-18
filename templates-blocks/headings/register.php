caa3eafe27a75a0a7d61b6f345fe033732f7dee5
Justin <justin.holloway@asu.edu>
2021-02-17 17:05:50 -0700

    Updated Register File
    Updated the following 
    - Added preview
    - Changed the block title, category, and keywords
    - Removed support for align and html buttons in editor tool bar
    - updated the block preview to match the block

--- templates-blocks/headings/register.php
+++ templates-blocks/headings/register.php
@@ -21,26 +21,24 @@
 acf_register_block_type(
 	array(
 		'name'              => 'headings', // internal name, like a slug.
-		'title'             => __( 'UDS Headings Block', 'uds-wordpress-theme' ), // name the user will see.
+		'title'             => __( 'UDS Heading', 'uds-wordpress-theme' ), // name the user will see.
 		'description'       => __( 'A UDS heading block based on WS2.0.', 'uds-wordpress-theme' ), // description the user will see.
 		'icon'              => '<svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" role="img" aria-hidden="true" focusable="false"><path d="M6.2 5.2v13.4l5.8-4.8 5.8 4.8V5.2z"></path></svg>', // Dashicon, or custom SVG code, for the icon.
 		'render_template'   => 'templates-blocks/headings/headings.php', // location of the block's template.
-		'category'          => 'layout', // category this block appears in.
-		'keywords'          => array( 'callout', 'quote', 'text' ),
+		'category'          => 'text', // category this block appears in.
+		'keywords'          => array( 'heading', 'title', 'highlight' ),
 		'supports'          => array(
-			'align' => true, // Remove the align button in the editor toolbar.
-			'html' => true, // Allows the use of html editor.
+			'align' => false, // Disable the align button in the editor toolbar.
+			'html' => false, // Disable the use of html editor.
 		),
 		'mode'              => 'edit', // make this block default to full edit mode when added to the page.
 		'example'  => array(
 			'attributes' => array(
 				'mode' => 'preview',
 				'data' => array(
-					'title'                   => 'A Headings Block',
-					'background_style'        => 'network-white',
-					'container_type'          => 'container-fluid',
-					'add_inner_container'     => true,
-					'content'                 => 'Lorem ipsum sit dolor amet.',
+					'level'                   => '1',
+					'text_highlight'        => 'highlight-gold',
+					'text'          => 'your heading text',
 				),
 			),
 		),
