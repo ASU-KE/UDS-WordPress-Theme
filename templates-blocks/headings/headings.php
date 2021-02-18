eee1fa9840e338b282e7d9be96518bd6350f327d
Justin <justin.holloway@asu.edu>
2021-02-17 17:12:31 -0700

    Cleaned code and added comments

--- templates-blocks/headings/headings.php
+++ templates-blocks/headings/headings.php
@@ -7,15 +7,20 @@
  * This is a UDS heading block to replace the standard heading block.
  */
 
+ // Setup our variables.
 $start_tag = '<h' . get_field( 'level' ) . '>';
 $end_tag = '</h' . get_field( 'level' ) . '>';
 $highlight_class = get_field( 'text_highlight' );
+
+// Dont use highlight class if none is selected.
 if ( 'none' == $highlight_class ) {
 	$highlight_class = '';
 }
 
-$header_text = get_field( 'text' );
+// Sanitize header text field.
+$header_text = sanitize_text_field( get_field( 'text' ) );
 
+// Construct the heading tag.
 echo $start_tag . ' <span class="' . $highlight_class . '">' . $header_text . '</span>' . $end_tag;
 ?>
 
