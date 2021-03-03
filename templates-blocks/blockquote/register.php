7d2bc659b4660a2b0c68d5b5c57bbdc1cf55a1ca
Walter McConnell <Walter.McConnell@asu.edu>
2021-03-03 10:39:10 -0700

    Clean up register file
    Multiple changes/fixes here:
    
    * Changed slug to 'uds-blockquote' for consistency with (most) of our other blocks
    * Changed keywords for better searching
    * Updated preview data
    * Updated comments

--- templates-blocks/blockquote/register.php
+++ templates-blocks/blockquote/register.php
@@ -3,43 +3,41 @@
  * Block Registration
  *
  * Block name: Blockquote
- * Author: Walt
  * Version: 1.0
  *
+ * @author Walt, Earl, Justin
  * @package UDS WordPress Theme
  *
- * Notes: A blockquote to be used within the UDS Wordpress theme. This block is attempting to use the settings we are
- * most likely to want, but you can find all of the available settings in the ACF
- * documentation here:
- * https://www.advancedcustomfields.com/resources/acf_register_block_type/
+ * Notes: A blockquote to be used within the UDS WordPress theme. It is a
+ * combination of the UDS blockquote (which was not exactly well defined),
+ * and the UDS Testimonial. It's basically block quote at large screens,
+ * and the testimonial styling at mobile sizes. Supports:
  *
- * Note: for the icon, you can use any Dashicon:
- * https://developer.wordpress.org/resource/dashicons, OR an actual <SVG></SVG> tag
- * with all the required content. In this example, I'm using a dashicon.
+ *  image or no image, adjusting layout accordingly
+ *  setting UDS approved background colors
+ *  restricting large quotation mark color based on background color
  */
 
 acf_register_block_type(
 	array(
-		'name'              => 'blockquote', // internal name, like a slug.
+		'name'              => 'uds-blockquote', // internal name, like a slug.
 		'title'             => __( 'UDS Blockquote', 'uds-wordpress-theme' ), // name the user will see.
-		'description'       => __( 'A blockquote block to be used in the UDS theme', 'uds-wordpress-theme' ), // description the user will see.
+		'description'       => __( 'UDS styled block quotes, with or without an image', 'uds-wordpress-theme' ), // description the user will see.
 		'icon'              => 'format-quote', // Dashicon, or custom SVG code, for the icon.
 		'render_template'   => 'templates-blocks/blockquote/blockquote.php', // location of the block's template.
 		'category'          => 'layout', // category this block appears in.
-		'keywords'          => array( 'callout', 'quote', 'text' ),
+		'keywords'          => array( 'blockquote', 'block', 'quote' ),
 		'supports'          => array(
 			'align' => false, // Remove the align button in the editor toolbar.
 		),
-		'mode'              => 'preview', // make this block default to full edit mode when added to the page.
+		'mode'              => 'preview', // make this block default to preview mode.
 		'example'  => array(
 			'attributes' => array(
 				'mode' => 'preview',
 				'data' => array(
-					'title'                   => 'A Block Quote Block',
-					'background_style'        => 'network-white',
-					'container_type'          => 'container-fluid',
-					'add_inner_container'     => true,
-					'content'                 => 'Lorem ipsum sit dolor amet.',
+					'quote_text'  => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.',
+					'name'        => 'Name of person',
+					'description' => 'description here',
 				),
 			),
 		),
