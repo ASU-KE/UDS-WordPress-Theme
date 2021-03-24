9becf7fd1a37333a8212fa0c648d7099be47f80a
Walter McConnell <Walter.McConnell@asu.edu>
2021-03-24 16:44:06 -0700

    Add Multiple Buttons and Enhanced Highlighting
    Updates to allow for multiple buttons in the hero area, and for highlighting only part of the title.
    
    Includes a change to the button group to remove the instruction text (for better display in the repeater field) and apply the '-collapsed-target' class to the Link field so something shows up in 'collapsed' rows in the editor.

--- templates-global/hero.php
+++ templates-global/hero.php
@@ -9,11 +9,15 @@
 // Exit if accessed directly.
 defined( 'ABSPATH' ) || exit;
 
-// Get asset value based on choice of URL or local file.
+/**
+ * Determine the source of our main hero media. Options are 'url', for external images
+ * from a full URL, or a file from the site's Media Library. We check the user's choice,
+ * then pull the value from the appropriate field.
+ */
 $media_source = get_field( 'media_source' );
 switch ( $media_source ) {
 	case 'url':
-		$hero_asset_url = get_field( 'hero_asset_url' );
+		$hero_asset_url = esc_url( get_field( 'hero_asset_url' ) );
 		break;
 
 	default:
@@ -21,25 +25,37 @@ switch ( $media_source ) {
 		break;
 }
 
-$hero_size = get_field( 'hero_size' );
-$hero_title = get_field( 'hero_title' );
-$hero_highlight = get_field( 'hero_highlight' );
-$title_color = get_field( 'title_color' );
-$hero_text = get_field( 'hero_text' );
-$hero_call_to_action_url = get_field( 'hero_call_to_action_url' );
-$hero_call_to_action_text = get_field( 'hero_call_to_action_text' );
-$hero_call_to_action_color = get_field( 'call_to_action_color' );
-
-// Allow certain tags in the hero title and text.
+/**
+ * Allow certain tags within the title or body text of the hero area. This is a limited
+ * set of tags to support forcing line breaks when appropriate, and applying highlight
+ * classes via <span> tags.
+ */
 $hero_allowed_tags = array(
-	'br' => array(),
-	'p'  => array(),
+	'br'   => array(),
+	'p'    => array(),
+	'span' => array(
+		'class' => array(),
+	),
 );
 
 /**
- * If we don't have a highlight style, then determine the text color and
- * use that class for the title. Otherwise, use the selected highlight
- * class.
+ * Retrieve our field values. Note, we do not get button values here, as they are
+ * retrieved later on when rendered, as they involve looping through a repeater
+ * field.
+ */
+$hero_size = get_field( 'hero_size' );
+$hero_title = wp_kses( get_field( 'hero_title' ), $hero_allowed_tags );
+$hero_highlight = get_field( 'hero_highlight' );
+$title_color = get_field( 'title_color' );
+$hero_text = wp_kses( get_field( 'hero_text' ), $hero_allowed_tags );
+$single_word_highlight = sanitize_text_field( get_field( 'single_word_highlight' ) );
+
+
+
+/**
+ * If the user has selected a highlight color for the title, use that. If
+ * they didn't, use the value from the 'text color' field to determine the
+ * color of the title.
  */
 
  // holds our final title style.
@@ -50,15 +66,14 @@ if ( empty( $hero_highlight ) || 'none' == $hero_highlight ) {
 	if ( 'white' == $title_color ) {
 		$hero_title_style = 'text-white';
 	} else {
-		$title_title_style = 'text-black';
+		$hero_title_style = 'text-black';
 	}
 } else {
 	// we chose a highlight style, so just use that.
 	$hero_title_style = $hero_highlight;
 }
 
-
-// Deterine the hero size.
+// Determine the hero size class.
 switch ( $hero_size ) {
 	case 'small':
 		$hero_size_class = 'uds-hero-sm';
@@ -81,16 +96,58 @@ if ( ! empty( $hero_asset_url ) ) :
 <div class="uds-hero <?php echo $hero_size_class; ?>" >
 	<img srcset="<?php echo $hero_asset_url; ?>" src="<?php echo $hero_asset_url; ?>"/>
 	<div class="container uds-hero-container lazyloaded">
-		<?php
-		if ( ! empty( $hero_title ) ) :
-			?>
-		<div class="col-md-8">
-			<h1><span class="<?php echo $hero_title_style; ?>"><?php echo wp_kses( $hero_title, $hero_allowed_tags ); ?></span></h1>
-		</div>
-			<?php
-		endif;
 
-		if ( ! empty( $hero_text ) || ( ! empty( $hero_call_to_action_url ) && ! empty( $hero_call_to_action_text ) ) ) :
+		<?php
+		if ( ! empty( $hero_title ) ) {
+
+			/**
+			 * The user can choose between highlighting the whole title, just one word, or
+			 * having no title highlight. Based on that choice, we apply highlighting in
+			 * different way.
+			 */
+			$title_highlight_type = get_field( 'title_highlight_type' );
+
+			switch ($title_highlight_type) {
+				case 'all':
+					// Full title highlight. Wrap the entire text in a <span> of the chosen style.
+					echo '<h1><span class="' . $hero_title_style . '">' . $hero_title . '</span></h1>';
+					break;
+				case 'word':
+					/**
+					 * For single-word highlighting, we ensure the following:
+					 * - A word was actually provided
+					 * - That word is in the title
+					 * - You did not choose style 'none', even though you provided a word.
+					 *
+					 * If all those are true, then we replace the word with the same word wrapped in a span
+					 * of the approprite class. Otherwise, we fall back on the default title behavior.
+					 */
+					if( ! empty( $single_word_highlight ) && false !== strpos( $hero_title, $single_word_highlight ) && 'none' !== $hero_highlight) {
+						$title_string = str_replace(
+							$single_word_highlight,
+							'<span class="' . $hero_title_style . '">' . $single_word_highlight . '</span>',
+							$hero_title
+						);
+						echo '<h1 class="text-' . $title_color . '">' . $title_string . '</h1>';
+					}else{
+						// Display a normal title, but with the selected text color
+						echo '<h1><span class="' . $hero_title_style . '">' . $hero_title . '</span></h1>';
+					}
+					break;
+				case 'none':
+					// User has chosen the 'none' highlight. Ignore highlight settings and use title color only.
+					echo '<h1><span class="text-' . $title_color . '">' . $hero_title . '</span></h1>';
+					break;
+				default:
+					// Display a normal title, but with the selected text color
+					echo '<h1><span class="' . $hero_title_style . '">' . $title_string . '</span></h1>';
+					break;
+			}
+		}else{
+			wp_die( 'Title is empty!' );
+		}
+
+		if ( ! empty( $hero_text )  ) :
 			?>
 		<div class="uds-hero-text col-sm-12 col-md-7">
 			<?php
@@ -99,15 +156,38 @@ if ( ! empty( $hero_asset_url ) ) :
 				echo wp_kses( sprintf( $text, $hero_text ), $hero_allowed_tags );
 			}
 			?>
-			<?php
-			if ( ! empty( $hero_call_to_action_url ) && ! empty( $hero_call_to_action_text ) ) {
-				$text = '<a class="btn btn-%3$s" href="%1$s">%2$s</a>';
-				echo wp_kses( sprintf( $text, $hero_call_to_action_url, $hero_call_to_action_text, $hero_call_to_action_color ), wp_kses_allowed_html( 'post' ) );
-			}
-			?>
 		</div>
 			<?php
 		endif;
+		?>
+		<?php
+			// Render any buttons we have added to the hero area.
+			if( have_rows( 'hero_cta_buttons' ) ) {
+				echo '<div class="hero-buttons">';
+				while( have_rows( 'hero_cta_buttons' ) ) : the_row();
+					$size = get_sub_field( 'button_size' );
+					$color = get_sub_field( 'button_color' );
+					/**
+					 * The label, URL and target values are inside an ACF 'Link' field.
+					 * They do not have default values, like the other button fields,
+					 * so we check here to see if they've been set, and apply some defaults.
+					 */
+					if( get_sub_field( 'button_link' ) ) {
+						$button_link_data = get_sub_field( 'button_link' );
+						$button_label     = sanitize_text_field( $button_link_data['title'] );
+						$button_url       = esc_url( $button_link_data['url'] );
+						$button_target    = $button_link_data['target'];
+					}else{
+						$button_label  = 'Label Missing!';
+						$button_url    = '#';
+						$bugton_target = '_self';
+					}
+
+					$text = '<a class="btn btn-%3$s btn-%4$s mr-2 mb-2" href="%1$s">%2$s</a>';
+					echo wp_kses( sprintf( $text, $button_url, $button_label, $size, $color), wp_kses_allowed_html( 'post' ) );
+				endwhile;
+				echo '</div>';
+			}
 		?>
 	</div>
 </div>
