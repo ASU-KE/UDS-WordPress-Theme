396e4393f945473449385182b3ba703613530a84
Walter McConnell <Walter.McConnell@asu.edu>
2021-04-01 17:56:20 -0700

    Coding standardds

--- inc/tiny-mce.php
+++ inc/tiny-mce.php
@@ -1,9 +1,8 @@
 <?php
 
-add_filter( 'acf/fields/wysiwyg/toolbars' , 'uds_wp_toolbars'  );
-function uds_wp_toolbars( $toolbars )
-{
-	/**
+add_filter( 'acf/fields/wysiwyg/toolbars', 'uds_wp_toolbars' );
+function uds_wp_toolbars( $toolbars ) {
+	 /**
 	 * This function uses the toolbar preparation hook to register new toolbars
 	 * for use with ACF field groups. A toolbar is just a named collection of
 	 * existing buttons. Once created here, that new toolbar will be
@@ -24,8 +23,8 @@ function uds_wp_toolbars( $toolbars )
 	 * as needed.
 	 */
 
-	$toolbars['UDS-WP Simple' ] = array();
-	$toolbars['UDS-WP Simple' ][1] = array('bold' , 'italic' , 'underline', 'link' );
+	$toolbars['UDS-WP Simple'] = array();
+	$toolbars['UDS-WP Simple'][1] = array( 'bold', 'italic', 'underline', 'link' );
 
 	// return $toolbars - IMPORTANT!
 	return $toolbars;
--- templates-blocks/alert/alert.php
+++ templates-blocks/alert/alert.php
@@ -5,49 +5,49 @@
  * @package UDS WordPress Theme
  */
 
-    // set up the values used in the different types of alerts.
-    $settings = array(
-        'warning' => array(
-            'class' => 'warning',
-            'title' => 'Warning',
-            'icon'  => 'fa-bell',
-        ),
-        'success' => array(
-            'class' => 'success',
-            'title' => 'Success',
-            'icon'  => 'fa-check-circle',
-        ),
-        'info' => array(
-            'class' => 'info',
-            'title' => 'Information',
-            'icon'  => 'fa-info-circle',
-        ),
-        'danger' => array(
-            'class' => 'danger',
-            'title' => 'Error',
-            'icon'  => 'fa-exclamation-triangle',
-        )
-    );
+	// set up the values used in the different types of alerts.
+	$settings = array(
+		'warning' => array(
+			'class' => 'warning',
+			'title' => 'Warning',
+			'icon'  => 'fa-bell',
+		),
+		'success' => array(
+			'class' => 'success',
+			'title' => 'Success',
+			'icon'  => 'fa-check-circle',
+		),
+		'info' => array(
+			'class' => 'info',
+			'title' => 'Information',
+			'icon'  => 'fa-info-circle',
+		),
+		'danger' => array(
+			'class' => 'danger',
+			'title' => 'Error',
+			'icon'  => 'fa-exclamation-triangle',
+		),
+	);
 
-    // get the type of alert the user has selected.
-    $style = get_field( 'alert_style' );
-    $content = strip_tags( get_field( 'alert_content' ), '<a><em><u><strong>' );
+	// get the type of alert the user has selected.
+	$style = get_field( 'alert_style' );
+	$content = strip_tags( get_field( 'alert_content' ), '<a><em><u><strong>' );
 
-    // see if we have an entry for that. If not, choose information.
-    if ( ! array_key_exists( $style, $settings ) ) {
-        $style = 'info';
-    }
-?>
+	// see if we have an entry for that. If not, choose information.
+	if ( ! array_key_exists( $style, $settings ) ) {
+		$style = 'info';
+	}
+	?>
 
 <!-- alert block -->
-<div class="alert alert-block alert-<?php echo $settings[$style]['class']; ?>" role="alert">
-    <div class="alert-icon">
-        <span title="<?php echo $settings[$style]['title']; ?>" class="fa fa-icon <?php echo $settings[$style]['icon']; ?>"></span>
-    </div>
-    <div class="alert-content">
-        <?php echo $content; ?>
-    </div>
-    <div class="alert-close">
-        <button type="button" class="btn btn-circle btn-circle-alt-black close" aria-label="Close" onclick="event.target.parentNode.parentNode.parentNode.style.display='none';"><i class="fas fa-times"></i></button>
-    </div>
+<div class="alert alert-block alert-<?php echo $settings[ $style ]['class']; ?>" role="alert">
+	<div class="alert-icon">
+		<span title="<?php echo $settings[ $style ]['title']; ?>" class="fa fa-icon <?php echo $settings[ $style ]['icon']; ?>"></span>
+	</div>
+	<div class="alert-content">
+		<?php echo $content; ?>
+	</div>
+	<div class="alert-close">
+		<button type="button" class="btn btn-circle btn-circle-alt-black close" aria-label="Close" onclick="event.target.parentNode.parentNode.parentNode.style.display='none';"><i class="fas fa-times"></i></button>
+	</div>
 </div>
