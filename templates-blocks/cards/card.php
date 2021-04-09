c5d923e647c308021c4b6b75548dbd22bf45429d
Walter McConnell <Walter.McConnell@asu.edu>
2021-04-09 16:19:58 -0700

    Refactor
    Added some logic to ensure we have values (even if they're blank) if we can't get the field data.

--- templates-blocks/cards/card.php
+++ templates-blocks/cards/card.php
@@ -69,12 +69,20 @@ if ( ! empty( get_field( 'hover' ) ) ) {
 }
 
 // Get our image data from the array provided by ACF.
+$image_url = '';
+$image_alt = '';
+
 $image_data = get_field( 'image' );
+
+if ( ! empty( $image_data ) ) {
+	$image_url = $image_data['url'];
+	$image_alt = $image_data['alt'];
+}
 ?>
 
 <div class="card <?php echo $style_class; ?> <?php echo $orientation_class; ?> <?php echo $additional_classes; ?> <?php echo $hover_class; ?>">
 	<?php if ( 'image' == $header_style ) : ?>
-		<img class="card-img-top" src="<?php echo $image_data['url']; ?>" alt="<?php echo $image_data['alt']; ?>">
+		<img class="card-img-top" src="<?php echo $image_url; ?>" alt="<?php echo $image_alt; ?>">
 	<?php endif; ?>
 	<?php if ( 'icon' == $header_style ) : ?>
 		<i class="fas fa-<?php echo $icon_name; ?> fa-2x card-icon-top"></i>
