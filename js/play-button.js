64b3be24bfd5f696845ef8d4ea1ab7ea776ef19c
“ZainabD” <zalsidik@asu.edu>
2021-04-13 09:52:33 -0700

    fixed jscs and jshint errors

--- js/play-button.js
+++ js/play-button.js
@@ -1,18 +1,17 @@
 ( function( $ ) {
 
-	$('.side-tabs .tab-content .tab-pane button.btn.btn-circle').click(function(){
-		var main_contant = $(this).parent();
-		var iframe_video = main_contant.find('iframe');
-		main_contant.addClass('no-gradient-bg');
+	$( '.side-tabs .tab-content .tab-pane button.btn.btn-circle' ).click(function() {
+		var mainContant = $( this ).parent();
+		var iframeVideo = mainContant.find( 'iframe' );
+		mainContant.addClass( 'no-gradient-bg' );
 
-		      main_contant.find("img").fadeOut("slow");
-		      $(this).hide();
-		      iframe_video.attr(
-		        "src",
-		        iframe_video.attr("src").replace("autoplay=0", "autoplay=1")
+		      mainContant.find( 'img' ).fadeOut( 'slow' );
+		      $( this ).hide();
+		      iframeVideo.attr(
+		        'src',
+		        iframeVideo.attr( 'src' ).replace( 'autoplay=0', 'autoplay=1' )
 		      );
-		      iframe_video.attr("allow", "autoplay");
-
+		      iframeVideo.attr( 'allow', 'autoplay' );
 
 	});
 
