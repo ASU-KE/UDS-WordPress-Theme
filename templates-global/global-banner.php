313aa8e0a4d4b54ce6330bc0f4250a55db0a57b7
Walter McConnell <Walter.McConnell@asu.edu>
2021-04-15 13:26:20 -0700

    Fix Banner Widget Logic
    Since our banner widget allows for the user to put the widget in the widget area - but turn off the display of the widget. When that was the case, the surrounding markup was still renderedf (even though the widget was not), leaving an unnecessary empty <div> at the top of the content area. Worse, that <div> had some margin applied, and would move content down the page.
    
    Updated the logic in the template to only render markup when the widget is both active AND the user has specifically checked the box to display the widget.

--- templates-global/global-banner.php
+++ templates-global/global-banner.php
@@ -1,12 +1,18 @@
 <?php
 /**
  * Displays the global banner widget content if there's information to be displayed.
- * Normally gets called immediately after hero.php
+ * Normally gets called immediately after hero.php.
+ *
+ * It is possible, with our banner widget, to have the widget in the widget area but
+ * have it set to NOT display. To make sure we don't render any unwanted markup,
+ * we also check to be sure that the user wants to have the banner visible.
  *
  * @package uds-wordpress-theme
  */
 
-if ( is_active_sidebar( 'global-banner' ) ) : ?>
+$banner_is_active = get_field( 'uds_banner_active' );
+
+if ( is_active_sidebar( 'global-banner' ) && $banner_is_active ) : ?>
 
 	<div class="container">
 		<div class="row mb-12">
