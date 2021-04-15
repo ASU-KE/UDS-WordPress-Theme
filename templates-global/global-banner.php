ef8e9c27068f0fcf1597fc0a7449f06d5d2ee9eb
Walter McConnell <Walter.McConnell@asu.edu>
2021-04-15 13:48:02 -0700

    Remove Markup From Template Partial
    Because we cannot get the widget ID in the context of a basic template include, we could not query the value of the ACF field for showing/hiding the banner. Moved the surrounding markup from this template into the actual Banner widget class, where we do have access to the ACF field - and are already using it.
    
    This prevents the surrounding markup from being rendered in the case where the widget has been dragged to the widget area, but the user has chosen to hide the widget. Before, that would only hide the widget markup, leaving unwanted wrapping markup in the final page.

--- templates-global/global-banner.php
+++ templates-global/global-banner.php
@@ -3,23 +3,12 @@
  * Displays the global banner widget content if there's information to be displayed.
  * Normally gets called immediately after hero.php.
  *
- * It is possible, with our banner widget, to have the widget in the widget area but
- * have it set to NOT display. To make sure we don't render any unwanted markup,
- * we also check to be sure that the user wants to have the banner visible.
+ * Modified to remove markup that was here, and move that markup into the widget
+ * class, leaving just the `dynamic_sidebar` call here.
  *
  * @package uds-wordpress-theme
  */
 
-$banner_is_active = get_field( 'uds_banner_active' );
-
-if ( is_active_sidebar( 'global-banner' ) && $banner_is_active ) : ?>
-
-	<div class="container">
-		<div class="row mb-12">
-			<div class="col">
-				<?php dynamic_sidebar( 'global-banner' ); ?>
-			</div>
-		</div>
-	</div>
-
-<?php endif; ?>
+if( is_active_sidebar( 'global-banner') ) {
+	dynamic_sidebar( 'global-banner' );
+}
