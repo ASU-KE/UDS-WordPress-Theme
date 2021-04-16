c8f0e2c096f4697a6874d80daf5882693b844e9e
Walter McConnell <Walter.McConnell@asu.edu>
2021-04-15 17:11:59 -0700

    Remove Deprecated Functions
    According to the jQuery migrate site, the "scroll" and "resize" shortcut functions are deprecated. I updated the JS file to use the suggested on("resize") and on("scroll") methods instead.

--- js/back_to_top.js
+++ js/back_to_top.js
@@ -7,11 +7,11 @@
 
  ( function( $ ) {
 	 var vh = $( window ).height();
-	 $( window ).resize(function() {
-		 vh = $( window ).height();
-	 });
+	 $( window ).on( "resize", (function() {
+		vh = $( window ).height();
+	 }));
 
-	$( window ).scroll(function() {
+	$( window ).on( "scroll", function() {
 
     if ( $( this ).scrollTop() > vh + vh / 2 ) {
 
