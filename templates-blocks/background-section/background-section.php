0612afb0831c23def4dda05fc3da7818c3dc528c
Steve Ryan <steve.ryan@asu.edu>
2021-04-20 16:06:24 -0700

    Adding background image features. Fixing PHP notice.

--- templates-blocks/background-section/background-section.php
+++ templates-blocks/background-section/background-section.php
@@ -9,46 +9,49 @@ $choice = get_field( 'uds_background_section_choice' );
 $pattern = get_field( 'uds_background_section_pattern' );
 $preset = get_field( 'uds_background_section_preset' );
 $color = get_field( 'uds_background_section_color' );
+$innertext = get_field( 'uds_background_section_text' );
 $upload = get_field( 'uds_background_section_upload_url' );
 $innercolor = get_field( 'uds_background_inner_color' );
 $repeat = get_field( 'uds_background_image_repeat' );
 $position = get_field( 'uds_background_image_position' );
+$size = get_field( 'uds_background_image_size' );
 
 // Retrieve additional classes from the 'advanced' field in the editor.
+$additional_classes = '';
 if ( ! empty( $block['className'] ) ) {
 	$additional_classes = $block['className'];
 }
+
+// Add the .text-white modifier to additional classes.
+if ( 'text-white' === $innertext ) {
+	$additional_classes = 'text-white ' . $additional_classes;
+}
+
+// Add inner text color modifier to code if the text should be white.
+
 
 if ( $choice ) {
 
 	// Produce the correct classes for the <section> element and echo it.
 	// The $choice variable will be one of the following three values. It has a default value set by the ACF control.
-	if ( 'pattern' === $choice ) {
+	if ( 'color' === $choice ) {
+
+		if ( 'custom' === $preset ) {
+			// We're doing a custom background color.
+			echo '<section class="uds-section bg-color ' . $additional_classes . '" style="background-color: ' . $color . ';" >';
+		} else {
+			// Background colors via utility BS4 classes.
+			echo '<section class="uds-section bg-color ' . $preset . ' ' . $additional_classes . '" >';
+		}   
+	} else if ( 'pattern' === $choice ) {
 
 		// UDS Background patterns.
 		echo '<section class="uds-section bg ' . $pattern . ' ' . $additional_classes . '" >';
 
-	} else if ( 'color' === $choice ) {
-
-		if ( 'custom' === $preset ) {
-			// We're doing a custom background color.
-			echo '<section class="uds-section bg-color' . $additional_classes . '" style="background-color: ' . $color . ';" >';
-		} else {
-			// Background colors via utility BS4 classes.
-			echo '<section class="uds-section bg-color ' . $preset . ' ' . $additional_classes . '" >';
-		}
 	} else if ( 'upload' === $choice ) {
 
-		if ( 'no-repeat' === $position ) {
-			// Add the "/ cover " value to the generated string if we are not repeating the pattern.
-			$position = $position . ' / cover ';
-		} else {
-			// Add just an embedded space.
-			$position = $position . ' ';
-		}
-
 		// Build the inline style rule.
-		$inline_style = 'background: url(' . $upload . ') ' . $position . $repeat . ';';
+		$inline_style = 'background: url(' . $upload . ') ' . $position . ' / ' . $size . ' ' . $repeat . ';';
 
 		// Set the basic utility class + inner bg color as classes.
 
