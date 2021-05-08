16b2762c4c0e8a45c745d2f5a432c106e87074a9
Nathan Rollins <nathan.rollins@asu.edu>
2021-05-07 18:19:15 -0700

    Configure WP-REST API extensions for News posts

--- functions.php
+++ functions.php
@@ -34,9 +34,10 @@ $uds_wp_includes = array(
 	'/uds-blocks.php',                           // Custom blocks created with ACF Pro.
 	'/deprecated.php',                           // Load deprecated functions.
 	'/uds-contextual-help.php',                  // theme-specific context-senstive help tabs.
-	'/tiny-mce.php',                              // Create custom toolbars for the WYSIWYG editor in ACF.
+	'/tiny-mce.php',                             // Create custom toolbars for the WYSIWYG editor in ACF.
 	'/scroll-to-div.php',                        // Add animation to anchore scroll and offset -150.
-	'/post-post-type.php',                        // Create a custom post type for News.
+	'/post-post-type.php',                       // Create a custom post type for News.
+	'/wp-rest-api-extensions.php',               // Extend WP-REST API
 );
 
 foreach ( $uds_wp_includes as $file ) {
