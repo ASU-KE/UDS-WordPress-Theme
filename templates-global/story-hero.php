b0b5c859b82709bd037d5820e9619370c731c237
Steve Ryan <steve.ryan@asu.edu>
2021-05-11 00:36:00 -0700

    Adds support for large story hero image. Adds uds_posted_on string.

--- templates-global/story-hero.php
+++ templates-global/story-hero.php
@@ -13,44 +13,60 @@ defined( 'ABSPATH' ) || exit;
  * Determine if the user has chosen a background color or an image.
  */
 $bgchoice = get_field( 'uds_story_hero_background_choice' );
-// $bgcolor = get_field('uds_story_hero_background_color');
-// $bgimage = get_field('uds_story_hero_background_image');
+$hero_size = get_field ( 'uds_story_hero_background_image_size' );
+
+// Build initial section tag with correct hero size.
+$section_open = '<section class="uds-story-hero entry-header';
+$section_close = '">';
+
+if ( 'large' === $hero_size ) {
+	$section_open .= ' uds-story-hero-lg';
+}
 
 if ( 'image' === $bgchoice ) {
-	echo '<section class="uds-story-hero entry-header">';
+
+	echo $section_open . $section_close;
 
 	$bgimage = get_field( 'uds_story_hero_background_image' );
 	if ( ! empty( $bgimage ) ) {
 		echo '<img class="hero" src="' . esc_url( $bgimage['url'] ) . '" alt="' . esc_attr( $bgimage['alt'] ) . '" />';
 	}
+
 } else {
+
 	// 'color' === $bgchoice
 	$bgcolor = get_field( 'uds_story_hero_background_color' );
+
 	if ( ! empty( $bgcolor ) ) {
-		echo '<section class="uds-story-hero no-image ' . $bgcolor . ' entry-header">';
+
+		echo $section_open . ' no-image ' . $bgcolor . $section_close;
+
 	} else {
-		echo '<section class="uds-story-hero no-image bg-gray-3 entry-header">';
+
+		// This should never really be possible given a default ACF control value.
+		echo $section_open . $section_close;
+
 	}
 }
 
 // Return the rest of the section.
 ?>
 
-<div class="content">
-	<p class="meta entry-meta"><?php echo get_the_date( 'F d, Y' ); ?></p>
-	<?php
-	/*
-	  If ( function_exists( 'yoast_breadcrumb' ) ) {
-		echo '<div class="bg-white"><nav aria-label="breadcrumbs">';
-		$breadcrumb_output = yoast_breadcrumb( '<ol class="breadcrumb">', '</ol>', false );
-		echo preg_replace( '#</?span[^>]*>#is', '', $breadcrumb_output );
-		echo '</div></nav>';
-		*
-	}
-	*/
+	<div class="content">
+		<p class="meta entry-meta"><?php echo uds_wp_posted_on(); ?></p>
+		<?php
+		/*
+		If ( function_exists( 'yoast_breadcrumb' ) ) {
+			echo '<div class="bg-white"><nav aria-label="breadcrumbs">';
+			$breadcrumb_output = yoast_breadcrumb( '<ol class="breadcrumb">', '</ol>', false );
+			echo preg_replace( '#</?span[^>]*>#is', '', $breadcrumb_output );
+			echo '</div></nav>';
+			*
+		}
+		*/
 
-	?>
-	<?php the_title( '<h1 class="article entry-title">', '</h1>' ); ?>
+		?>
+		<?php the_title( '<h1 class="article entry-title">', '</h1>' ); ?>
 
-</div>
+	</div>
 </section>
