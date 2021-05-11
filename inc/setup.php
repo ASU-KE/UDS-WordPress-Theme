52301aba8e0344248900ed31dcd07dfb2bb76619
Steve Ryan <steve.ryan@asu.edu>
2021-05-10 23:26:50 -0700

    Relocated archive title filter function to setup.php
    Additional functions that filter page content/excerpts also live here. Reworked function to be child theme compliant.

--- inc/setup.php
+++ inc/setup.php
@@ -131,3 +131,31 @@ if ( ! function_exists( 'uds_wp_custom_excerpt_length' ) ) {
 	}
 
 }
+
+add_filter( 'get_the_archive_title' , 'uds_wp_custom_archive_title' );
+
+if ( ! function_exists( 'uds_wp_custom_archive_title' ) ) {
+
+	/**
+	 * Remove the default WordPress object label from archive title pages.
+	 * https://developer.wordpress.org/reference/hooks/get_the_archive_title/#user-contributed-notes
+	 *
+	 * @param string
+	 * @return string
+	 */
+
+	function uds_wp_custom_archive_title( $title ) {
+		if ( is_category() ) {
+			$title = single_cat_title( '', false );
+		} elseif ( is_tag() ) {
+			$title = single_tag_title( '', false );
+		} elseif ( is_author() ) {
+			$title = '<span class="vcard">' . get_the_author() . '</span>';
+		} elseif ( is_tax() ) { // for custom post types.
+			$title = sprintf( '%1$s', single_term_title( '', false ) );
+		} elseif ( is_post_type_archive() ) {
+			$title = post_type_archive_title( '', false );
+		}
+		return $title;
+	}
+}
