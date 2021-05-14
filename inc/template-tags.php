767252e1044b2fc1d6ca973d6878d8d5b310f9f9
Steve Ryan <steve.ryan@asu.edu>
2021-05-14 15:28:26 -0700

    Changes to uds_posted_on function for date byline.

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -15,10 +15,14 @@ if ( ! function_exists( 'uds_wp_posted_on' ) ) {
 	 * Prints HTML with meta information for the current post-date/time and author.
 	 */
 	function uds_wp_posted_on() {
-		$time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
-		if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
-			$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s"> (%4$s) </time>';
+		$time_string = '<span class="fas fa-calendar-day"></span><time class="entry-date published updated" datetime="%1$s">%2$s</time>';
+		if (is_user_logged_in()) {
+			// Modify the time stamp to include both sets of dates if there is a user logged in.
+			if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
+				$time_string = '<span class="fas fa-calendar-day"></span><time class="entry-date published" datetime="%1$s">%2$s</time>  <time class="updated text-gray-4 pl-1" datetime="%3$s"> (Last updated: %4$s) </time>';
+			}
 		}
+
 		$time_string = sprintf(
 			$time_string,
 			esc_attr( get_the_date( 'c' ) ),
@@ -26,25 +30,15 @@ if ( ! function_exists( 'uds_wp_posted_on' ) ) {
 			esc_attr( get_the_modified_date( 'c' ) ),
 			esc_html( get_the_modified_date() )
 		);
+
 		$posted_on   = apply_filters(
 			'uds_wp_posted_on',
 			sprintf(
-				'<span class="posted-on">%1$s <a href="%2$s" rel="bookmark">%3$s</a></span>',
-				esc_html_x( 'Posted on', 'post date', 'uds-wordpress-theme' ),
-				esc_url( get_permalink() ),
+				'<span class="posted-on">%1$s</span>',
 				apply_filters( 'uds_wp_posted_on_time', $time_string )
 			)
 		);
-		$byline      = apply_filters(
-			'uds_wp_posted_by',
-			sprintf(
-				'<span class="byline"> %1$s<span class="author vcard"> <a class="url fn n" href="%2$s">%3$s</a></span></span>',
-				$posted_on ? esc_html_x( 'by', 'post author', 'uds-wordpress-theme' ) : esc_html_x( 'Posted by', 'post author', 'uds-wordpress-theme' ),
-				esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ),
-				esc_html( get_the_author() )
-			)
-		);
-		echo $posted_on . $byline; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+		echo $posted_on; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 	}
 }
 
