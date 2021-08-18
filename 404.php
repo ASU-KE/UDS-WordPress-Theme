e7ef82d328f32c9cb41a4755095b5206c8b675c0
Walter McConnell <Walter.McConnell@asu.edu>
2021-08-18 11:16:41 -0700

    Remove 'wrapper' Div
    Another main difference between our page template and this one is that pages do not wrap all their content in a '.wrapper' div. Removing it got rid of the unwanted padding on the left and right on desktop and below.

--- 404.php
+++ 404.php
@@ -13,31 +13,29 @@ $image_404 = get_theme_mod( 'image_404' );
 get_header();
 ?>
 <main id="skip-to-content" <?php post_class(); ?>>
-	<div class="wrapper" id="404-wrapper">
-		<div class="v1-uds-hero uds-hero-lg hero-image">
-			<?php $image_404 = wp_kses( $image_404, wp_kses_allowed_html( 'post' ) ); ?>
-			<img src="<?php echo $image_404; ?>" alt="404 - Not Found" />
-			<div class="container v1-uds-hero-container">
-				<div class="container px-0 error-404 not-found">
-					<div class="row">
-						<div class="col col-lg-8">
-							<h1 class="heading heading-one col-md-12 px-0">
-								<span class="highlight highlight-gold highlight-heading-one">
-									<?php esc_html_e( '404 - Not Found', 'uds-wordpress-theme' ); ?>
-								</span>
-							</h1>
-						</div>
+	<div class="v1-uds-hero uds-hero-lg hero-image">
+		<?php $image_404 = wp_kses( $image_404, wp_kses_allowed_html( 'post' ) ); ?>
+		<img src="<?php echo $image_404; ?>" alt="404 - Not Found" />
+		<div class="container v1-uds-hero-container">
+			<div class="container px-0 error-404 not-found">
+				<div class="row">
+					<div class="col col-lg-8">
+						<h1 class="heading heading-one col-md-12 px-0">
+							<span class="highlight highlight-gold highlight-heading-one">
+								<?php esc_html_e( '404 - Not Found', 'uds-wordpress-theme' ); ?>
+							</span>
+						</h1>
 					</div>
-					<div class="row">
-						<div class="v1-uds-hero-text col col-lg-8 ">
-							<p><?php esc_html_e( 'It looks like nothing was found! Maybe try a search?', 'uds-wordpress-theme' ); ?></p>
-							<?php get_search_form(); ?>
-						</div><!-- .page-content -->
-					</div>
+				</div>
+				<div class="row">
+					<div class="v1-uds-hero-text col col-lg-8 ">
+						<p><?php esc_html_e( 'It looks like nothing was found! Maybe try a search?', 'uds-wordpress-theme' ); ?></p>
+						<?php get_search_form(); ?>
+					</div><!-- .page-content -->
 				</div>
 			</div>
 		</div>
-	</div><!-- #error-404-wrapper -->
+	</div>
 </main>
 <?php
 get_footer();
