fc26ddfa359fec48f495cc6406fda00acd0ec941
“ZainabD” <zalsidik@asu.edu>
2021-08-31 16:50:43 -0700

    removed video tabs and container wrapper from regular tabs

--- templates-blocks/tabs/tabs.php
+++ templates-blocks/tabs/tabs.php
@@ -7,38 +7,13 @@
  */
 
 $tabs_style = get_field( 'tabs_style' );
-$accordion_id = '';
 switch ( $tabs_style ) {
 	case 'accordion':
 		$accordion_id = 'Accordion' . date( 'hisa' );
-		$style_class = '';
-		$child_style_class = '';
-		$tab_pane_classes = '';
 		break;
 
-	case 'video_tabs':
-		$style_class = 'side-tabs';
-		$child_style_class = 'col-md-3 col-sm-12 float-right';
-		$tab_pane_classes = 'col-md-9 col-sm-12 float-left card';
-		break;
-
-	/*case 'dark_background':
-		$style_class = 'background-darkmode';
-		$child_style_class = 'darkmode';
-		$tab_pane_classes = '';
-		break;
-
-
-	case 'gray_background':
-		$style_class = 'background-smokemode';
-		$child_style_class = 'smokemode';
-		$tab_pane_classes = '';
-		break;
-*/
 	default:
-		$style_class = '';
-		$child_style_class = '';
-		$tab_pane_classes = '';
+	$accordion_id = '';
 		break;
 }
 
@@ -64,14 +39,6 @@ if ( have_rows( 'tabs' ) ) {
 				$tab_content_type = get_sub_field( 'tab_content' );
 		if ( ! $tab_content_type ) {$tab_content_type = 'post_content';}
 				$tab_content = get_sub_field( $tab_content_type );
-		$video_poster_on_content = '';
-		if ( 'video_tabs' == $tabs_style ) {
-			$video_poster = get_the_post_thumbnail( $tab_content->ID );
-			$video_poster_alt = get_post_meta( get_post_thumbnail_id( $tab_content->ID ), '_wp_attachment_image_alt', true );
-			$video_poster_on_content = '<button type="button" class="btn btn-circle btn-circle-alt-white btn-circle-large"><i class="fas fa-play"></i><span class="sr-only">Play</span></button>' . $video_poster;
-			$tab_content->post_content = '<iframe width="560" height="315" src="' . strip_tags( $tab_content->post_content ) . '?autoplay=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen=""></iframe>';
-
-		}
 
 
 
@@ -88,9 +55,9 @@ if ( have_rows( 'tabs' ) ) {
 			$this_tab_content .= apply_filters('the_content', $tab_content1);
 			$this_tab_content .='</div>';
 			$tab_content = $this_tab_content;
-			//$tab_content = str_replace(']]>', ']]&gt;', $tab_content);
-		  //$tab_content=get_post_field('post_content', $tab_ID);
-			$tab_content .='<span class="edit-link"><a class="post-edit-link btn btn-md btn-maroon" href="'.get_edit_post_link($tab_ID).'">Edit</a></span>';
+      if (get_edit_post_link($tab_ID)){
+			$tab_content .='<span class="edit-link"><a class="post-edit-link btn btn-sm btn-dark" href="'.get_edit_post_link($tab_ID).'">Edit tab content</a></span>';
+		  }
 		} else {
       $current_time=gettimeofday(true);
 			$current_time=str_replace('.', '', $current_time);
@@ -122,7 +89,7 @@ $accordion_card_style = get_sub_field( 'accordion_card_style' );
 
 
 				   $tab_header[] = '<a class="nav-item nav-link ' . $is_active . '" id="tab-header-' . $tab_content_id . '" data-toggle="tab" href="#tab-content-' . $tab_content_id . '" role="tab" aria-controls="tab-content-' . $tab_content_id . '" aria-selected="' . $aria_selected . '">' . $tab_title . '</a>';
-				   $tab_post_content[] = '<div class="tab-pane fade ' . $show_classes . '" id="tab-content-' . $tab_content_id . '" role="tabpanel" aria-labelledby="tab-header-' . $tab_content_id . '">' . $video_poster_on_content . $tab_content . '</div>';
+				   $tab_post_content[] = '<div class="tab-pane fade ' . $show_classes . '" id="tab-content-' . $tab_content_id . '" role="tabpanel" aria-labelledby="tab-header-' . $tab_content_id . '">' . $tab_content . '</div>';
 
 
 		if ( 'accordion' == $tabs_style ) {
@@ -145,10 +112,7 @@ $accordion_card_style = get_sub_field( 'accordion_card_style' );
 ?>
 
 <?php if ( 'accordion' != $tabs_style ) { ?>
-<div class="container <?php echo $style_class; ?>">
-	<div class="row">
-		<div class="col">
-<nav class="uds-tabbed-panels <?php echo $child_style_class; ?>">
+<nav class="uds-tabbed-panels">
 
 	<div class="nav nav-tabs" id="nav-tab-<?php echo str_replace( ' ', '_', $tab_title ); ?>" role="tablist" data-scroll-position="0">
 	<?php
@@ -167,15 +131,12 @@ $accordion_card_style = get_sub_field( 'accordion_card_style' );
 			<span class="sr-only">Next</span>
 		  </a>
 </nav>
-<div class="tab-content <?php echo $tab_pane_classes; ?>" id="nav-tabContent-<?php echo str_replace( ' ', '_', $tab_title ); ?>">
+<div class="tab-content" id="nav-tabContent-<?php echo str_replace( ' ', '_', $tab_title ); ?>">
 	<?php
 	foreach ( $tab_post_content as $this_tab_post_content ) {
 		echo $this_tab_post_content;
 	}
 	?>
-</div>
-</div>
-</div>
 </div>
 <?php } else { ?>
 
