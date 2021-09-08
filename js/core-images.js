ac71260f4a012de91cb41ad60e4e7b568b66a0cd
“ZainabD” <zalsidik@asu.edu>
2021-09-08 14:26:07 -0700

    updated js function for core/image to update edit and save functions

--- js/core-images.js
+++ js/core-images.js
@@ -19,7 +19,33 @@ wp.domReady( function() {
 
 		wp.blocks.unregisterBlockStyle( 'core/image', 'rounded' );
 
+		const { __, setLocaleData } = wp.i18n;
+		const { registerBlockType } = wp.blocks;
+		//import ServerSideRender from '@wordpress/server-side-render';
 
+		registerBlockType( 'core/image', {
+		    title: __( 'SimpleTOC', 'simpletoc' ),
+		    icon: 'list-view',
+		    category: 'layout',
+		    edit: function( props ) {
+		        return (
+
+												<div className="nice-img">
+        <figure className="wp-block-image">
+          <a data-lightbox="gallery" href={attributes.imageHref}>
+            {cardImage(attributes.imageUrl, attributes.imageAlt)}
+          </a>
+          <figcaption>{attributes.title}</figcaption>
+        </figure>
+      </div>
+
+
+		        );
+		    },
+		    save: props => {
+		        return null;
+		    },
+		} );
 
 
 });
