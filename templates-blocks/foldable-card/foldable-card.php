edbeb2a34bbe89b0d0328d02da465e6995135ca4
“ZainabD” <zalsidik@asu.edu>
2021-09-17 09:18:51 -0700

    fixed the jq error for accordions

--- templates-blocks/foldable-card/foldable-card.php
+++ templates-blocks/foldable-card/foldable-card.php
@@ -75,7 +75,7 @@ $( document ).ready(function() {
 	$('.accordion').each(function(){
 		var parent_id= $(this).attr('id');
 		//$(this).find('.card.card-foldable .card-body').css('background','red');
-		$(this).find('.card.card-foldable .card-body').attr('data-parent', parent_id);
+		$(this).find('.card.card-foldable .card-body').attr('data-parent', '#'+parent_id);
 	});
 });
 } )( jQuery );
