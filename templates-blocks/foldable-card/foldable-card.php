c77c62988c9085d6a5c3ce8a20e23fc8af8acec8
“ZainabD” <zalsidik@asu.edu>
2021-09-20 10:29:13 -0700

    updated script on foldable cards

--- templates-blocks/foldable-card/foldable-card.php
+++ templates-blocks/foldable-card/foldable-card.php
@@ -74,10 +74,8 @@ echo '</div>
 $( document ).ready(function() {
 	$('.accordion').each(function(){
 		var parent_id= $(this).attr('id');
-		//$(this).find('.card.card-foldable .card-body').css('background','red');
 		$(this).find('.card.card-foldable .card-body').attr('data-parent', '#'+parent_id);
 	});
 });
 } )( jQuery );
-
 </script>
