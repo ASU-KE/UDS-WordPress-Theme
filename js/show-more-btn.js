1522e28a31fa488b0e6e7b8c48d27d880eb62e06
“ZainabD” <zalsidik@asu.edu>
2021-10-13 11:00:30 -0700

    fix the js click function for show more and less buttons

--- js/show-more-btn.js
+++ js/show-more-btn.js
@@ -8,26 +8,27 @@
 	( function( $ ) {
 
 $( document ).ready(function() {
-			$( '.uds-show-more-btn a' ).click(function() {
-				$(this).parent().addClass('uds-show-less-btn');
-       $(this).parent().removeClass('uds-show-more-btn');
-			 $(this).html('<i class="fas fa-chevron-up"></i>');
-			 var this_btn=$(this).parent().clone();
-			 $('html, body').animate({ scrollTop:  $(this).parent().offset().top - 150 }, 'slow');
-			 $(this).parent().parent().append(this_btn);
-			 $(this).parent().remove();
-			});
 
-			$( '.uds-show-less-btn a' ).click(function() {
-				$(this).parent().addClass('uds-show-more-btn');
-       $(this).parent().removeClass('uds-show-less-btn');
-			 var this_title=$(this).parent().attr('title');
-			 $(this).html(this_title+'<i class="fas fa-chevron-down"></i>');
-			 var this_btn=$(this).parent().clone();
-			 $('html, body').animate({ scrollTop:  $(this).parent().offset().top + 150 }, 'slow');
-			 $(this).parent().parent().append(this_btn);
-			 $(this).parent().remove();
-			});
+	$( '.uds-show-btn' ).click(function() {
+		if ($(this).hasClass("uds-show-less-btn")){
+			$(this).css('opacity','0');
+			$(this).addClass('uds-show-more-btn');
+			$(this).removeClass('uds-show-less-btn');
+			var this_title=$(this).attr('btn-title');
+			$(this).find('a.btn').html(this_title+' <i class="fas fa-chevron-down"></i>');
+			$('html, body').animate({ scrollTop:  $(this).parent().offset().top -150 }, 'slow');
+			$(this).fadeTo( "slow", 1 );
+
+		}else{
+			$(this).addClass('uds-show-less-btn');
+			$(this).parent().addClass('uds-show-less-container');
+			$(this).removeClass('uds-show-more-btn');
+			$(this).find('a.btn').html('<i class="fas fa-chevron-up"></i>');
+			$('html, body').animate({ scrollTop:  $(this).prev().offset().top - 150 }, 'slow');
+		}
+	});
+
+
 
 });
 	 } )( jQuery );
