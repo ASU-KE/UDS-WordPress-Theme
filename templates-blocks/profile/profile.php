5dd44b1e88922e241594e4c37a678d759b3ab030
Walter McConnell <Walter.McConnell@asu.edu>
2021-10-26 13:23:53 -0700

    Create Initial Files
    Created the files required to build a block, and updated our `uds-blocks.php` file to make sure our new block is loaded.

---
+++ templates-blocks/profile/profile.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * Do some stuff here.
+ */
+
+?>
+
+<div class="profile profile-type-standard">
+	<div class="profile-row">
+		<div class="profile-photo-column">
+			<a href="/profile/hbreetz">
+				<img class="pictureOriginal" src="https://webapp4.asu.edu/photo-ws/directory_photo/1180093" alt="profile picture for Hanna Breetz">
+			</a>
+		</div>
+		<div class="profile-bio-column">
+			<h3 class="profile-name"><a href="/profile/hbreetz">Hanna Breetz</a></h3>
+			<div class="profile-title">
+				<p class="titleOriginal">Assistant Professor</p>
+			</div>
+			<div class="profile-contact-row">
+				<div class="">
+					<p>
+						<a class="linkOriginal" href="mailto:Hanna.Breetz@asu.edu">Hanna.Breetz@asu.edu</a>
+					</p>
+				</div>
+				<div class="">
+					<p>
+						<a class="" href="tel:480-727-0408">480-727-0408</a>
+					</p>
+				</div>
+				<!--
+				<div class="">
+					<p>
+						<div>
+							<br>
+						</div>
+					</p>
+				</div>
+-->
+			</div>
+			<p>Hanna Breetz is a political scientist who studies the political economy of energy transitions.</p>
+		</div>
+	</div>
+</div>
