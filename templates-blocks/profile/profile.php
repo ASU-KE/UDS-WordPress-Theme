989178e6face25ee4552fe2c412ccb339504a5cc
Walter McConnell <Walter.McConnell@asu.edu>
2021-10-29 11:27:00 -0700

    Refactor the ACF Variable Process
    Switched from simple get_form() and the_form() to create local variables for the various fields.

--- templates-blocks/profile/profile.php
+++ templates-blocks/profile/profile.php
@@ -1,59 +1,117 @@
 <?php
 
 /**
- * Do some stuff here.
+ * Retrieve values from ACF. To make cleaner, easier to read logic in the
+ * output area below, we're grabbing all of our values first to allow us
+ * to manipulate them if needed before we present them.
  */
 
 /**
- * Image data returns an array, so we can't use the normal get_field() or
- * the_field() functions. We grab the image data array here, and echo out
- * the parts we want later.
+ * Person Data
  */
-$image_data = get_field('uds_profile_image');
+$image_data   = get_field( 'uds_profile_image');
+$person_name  = get_field( 'uds_profile_name' );
+$person_title = get_field( 'uds_profile_title' );
+$person_email = get_field( 'uds_profile_email' );
+$person_phone = get_field( 'uds_profile_phone' );
+$person_url   = get_field( 'uds_profile_url' );
+$person_text  = get_field( 'uds_profile_text' );
 
-// Get the orientation value and set a class if needed.
+/**
+ * Settings Data
+ *
+ * Values associated with the Display Options tab. Controls the orientation
+ * of the profile, and allows users to turn elements on and off.
+ */
+
+// Get orientation value and set a class if we want a vertical profile.
 $orientation = get_field( 'uds_profile_orientation' );
 $orientation_class = '';
 if ( 'vertical' === $orientation ) {
 	$orientation_class = 'vertical';
 }
 
+/**
+ * Element toggle values. Set a local variable based on whether an element is
+ * set to be shown or hidden. The way ACF does this with our Checkbox field is
+ * to return an array of ONLY the selected values (i.e. boxes that were checked).
+ *
+ * To know if we want to display something, we need to check to see if the array
+ * contains the value for any possible checkbox, and create a local variable.
+ */
+
+// 1. Get the actual array of checked items.
+$elements = get_field('uds_profile_elements');
+
+// 2. Look for each possible value, and set a local variable.
+$show_image        = $elements && in_array( 'show_image', $elements ) ? true : false;
+$show_name         = $elements && in_array( 'show_name', $elements ) ? true : false;
+$show_title        = $elements && in_array( 'show_title', $elements ) ? true : false;
+$show_contact_row  = $elements && in_array( 'show_contact_row', $elements ) ? true : false;
+$show_profile_text = $elements && in_array( 'show_profile_text', $elements ) ? true : false;
+
+/**
+ * Other variables
+ */
+
+// If no URL was provided, do not make the image or name an actual link
+$link_image_and_name = $person_url && '' !== $person_url ? true : false;
+
+// Add class to remove the bottom border when it's been turned off.
+$show_bottom_border  = get_field( 'uds_profile_bottom_border' );
+if( $show_bottom_border ) {
+	$bottom_border_class = '';
+}else{
+	$bottom_border_class = 'no-border';
+}
+
 ?>
 
 <div class="profile profile-type-standard">
-	<div class="profile-row  <?php echo $orientation_class; ?>">
-		<div class="profile-photo-column">
-			<a href="<?php the_field('uds_profile_url'); ?>">
-				<img class="pictureOriginal" src="<?php echo $image_data['url']; ?>" alt="<?php echo $image_data['alt']; ?>">
-			</a>
-		</div>
+	<div class="profile-row  <?php echo $orientation_class; ?> <?php echo $bottom_border_class; ?>">
+		<?php if( $show_image ): ?>
+			<div class="profile-photo-column">
+				<?php if( $link_image_and_name ): ?>
+					<a href="<?php the_field('uds_profile_url'); ?>">
+						<img class="pictureOriginal" src="<?php echo $image_data['url']; ?>" alt="<?php echo $image_data['alt']; ?>">
+					</a>
+				<?php else: ?>
+					<img class="pictureOriginal" src="<?php echo $image_data['url']; ?>" alt="<?php echo $image_data['alt']; ?>">
+				<?php endif; ?>
+			</div>
+		<?php endif; ?>
 		<div class="profile-bio-column">
-			<h3 class="profile-name"><a href="<?php the_field('uds_profile_url'); ?>"><?php the_field('uds_profile_name'); ?></a></h3>
-			<div class="profile-title">
-				<p class="titleOriginal"><?php the_field('uds_profile_title'); ?></p>
-			</div>
-			<div class="profile-contact-row">
-				<div class="">
-					<p>
-						<a class="linkOriginal" href="mailto:<?php the_field('uds_profile_email'); ?>"><?php the_field('uds_profile_email'); ?></a>
-					</p>
+			<?php if( $show_name ): ?>
+				<h3 class="profile-name">
+					<?php if( $link_image_and_name ): ?>
+						<a href="<?php echo( esc_url( $person_url ) ); ?>"><?php echo $person_name; ?></a>
+					<?php else: ?>
+						<?php echo $person_name; ?>
+					<?php endif; ?>
+				</h3>
+			<?php endif; ?>
+			<?php if( $show_title ): ?>
+				<div class="profile-title">
+					<p class="titleOriginal"><?php echo $person_title; ?></p>
 				</div>
-				<div class="">
-					<p>
-						<a class="phoneOriginal" href="tel:<?php the_field('uds_profile_phone'); ?>"><?php the_field('uds_profile_phone'); ?></a>
-					</p>
+			<?php endif; ?>
+			<?php if( $show_contact_row ): ?>
+				<div class="profile-contact-row">
+					<div class="">
+						<p>
+							<a class="linkOriginal" href="mailto:<?php echo $person_email; ?>"><?php echo $person_email; ?></a>
+						</p>
+					</div>
+					<div class="">
+						<p>
+							<a class="phoneOriginal" href="tel:<?php echo $person_phone; ?>"><?php echo $person_phone; ?></a>
+						</p>
+					</div>
 				</div>
-				<!--
-				<div class="">
-					<p>
-						<div>
-							<br>
-						</div>
-					</p>
-				</div>
-				-->
-			</div>
-			<p class="profile-text"><?php the_field('uds_profile_text'); ?></p>
+			<?php endif; ?>
+			<?php if( $show_profile_text ): ?>
+				<p class="profile-text"><?php echo $person_text; ?></p>
+			<?php endif; ?>
 		</div>
 	</div>
 </div>
