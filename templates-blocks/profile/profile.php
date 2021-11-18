d091b2e9db360b7c18cd176227126d55b74ef985
Walter McConnell <Walter.McConnell@asu.edu>
2021-11-18 16:37:53 -0700

    Revert to Plugin Profile Style
    Go back to using the classes and styling produced by our current profile plugin.

--- templates-blocks/profile/profile.php
+++ templates-blocks/profile/profile.php
@@ -29,8 +29,9 @@ if ( isset( $block['className'] ) && ! empty( $block['className'] ) ) {
 }
 
 // Add our bottom-border class when requested.
-if ( $horizontal_rule ) {
-	$additional_classes .= ' profile-bottom-border';
+$border_override_class = '';
+if ( ! $horizontal_rule ) {
+	$border_override_class .= 'no-bottom-border';
 }
 
 /**
@@ -81,106 +82,54 @@ if( empty( $person_email) && empty( $person_phone) && empty( $person_street_addr
 
 // Render the block.
 ?>
-<div class="uds-person-profile <?php echo $orientation_class; ?> <?php echo $additional_classes; ?>">
-	<?php if( ! empty( $image_data ) ): ?>
-		<?php if( 'image' === $profile_link_type ): ?>
-			<a class="img-link" href="<?php echo $person_url; ?>" target="_blank" rel="noopener noreferrer">
-		<?php endif; ?>
-		<img
-		alt="<?php echo $image_data['alt'];?>"
-		class="profile-img"
-		src="<?php echo $image_data['url'];?>"
-		/>
-		<?php if( 'image' === $profile_link_type ): ?>
+
+<!-- Actually Render the Block (compatible version) -->
+<div class="profile profile-type-standard <?php echo $additional_classes; ?>">
+	<div class="profile-row <?php echo $border_override_class; ?>">
+		<div class="profile-photo-column">
+			<a href="" target="_blank" rel="noopener noreferrer">
+				<img class="pictureOriginal" src="<?php echo $image_data['url'];?>" alt="profile picture for <?php echo $person_name; ?>">
 			</a>
-		<?php endif; ?>
-	<?php endif; ?>
-
-	<div class="person  <?php echo $orientation_class; ?>">
-		<h3 class="person-name">
-			<?php if( 'image' === $profile_link_type ): ?>
+		</div>
+		<div class="profile-bio-column">
+			<h3 class="profile-name">
 				<a href="<?php echo $person_url; ?>" target="_blank" rel="noopener noreferrer">
-			<?php endif; ?>
-			<?php echo $person_name; ?>
-			<?php if( 'image' === $profile_link_type ): ?>
+					<?php echo $person_name; ?>
 				</a>
-			<?php endif; ?>
-		</h3>
-		<?php if( ! empty( $person_title ) ): ?>
-			<h4 class="person-profession"><?php echo $person_title; ?></h4>
-		<?php endif; ?>
-
-		<?php if( $render_contact_row ): ?>
-			<ul class="person-contact-info">
-				<?php if( ! empty( $person_email ) ): ?>
-				<li>
-					<a
-					aria-label="Email user"
-					href="mailto:email@asu.edu"
-					>
-						<?php echo $person_email; ?>
-					</a>
-				</li>
-				<?php endif; ?>
-
-				<?php if( ! empty( $person_phone ) ): ?>
-				<li>
-					<a
-					aria-label="Call user"
-					href="tel:<?php echo $person_phone; ?>">
-					<?php echo $person_phone; ?>
-					</a>
-				</li>
-				<?php endif; ?>
-
-				<?php if( ! empty( $person_street_address ) && ! empty( $person_city_state_zip ) ): ?>
-				<li>
-					<a aria-label="See user address" href="#" >
-						<address className="person-address">
-							<span className="person-street"><?php echo $person_street_address; ?></span>
-							<span className="person-city"><?php echo $person_city_state_zip; ?></span>
-						</address>
-					</a>
-				</li>
-				<?php endif; ?>
-			</ul>
-		<?php endif; ?>
-
-		<?php if( ! empty( $person_text ) ): ?>
-			<p class="person-description">
+			</h3>
+			<div class="profile-title">
+				<p class="titleOriginal">
+					<?php echo $person_title; ?>
+				</p>
+			</div>
+			<div class="profile-contact-row">
+				<div class="">
+					<p>
+						<a class="linkOriginal" href="mailto:BRENDA.DAY@asu.edu">
+							<?php echo $person_email; ?>
+						</a>
+					</p>
+				</div>
+				<div class="">
+					<p>
+						<a class="" href="tel:<?php echo $person_phone;?>">
+							<?php echo $person_phone; ?>
+						</a>
+					</p>
+				</div>
+				<div class="">
+						<div>
+							<p>
+								<?php echo $person_street_address; ?>
+								<br>
+								<?php echo $person_city_state_zip; ?>
+							</p>
+						</div>
+				</div>
+			</div>
+			<p>
 				<?php echo $person_text; ?>
 			</p>
-		<?php endif; ?>
-		<?php if( 'link' === $profile_link_type ): ?>
-			<p><a href="<?php echo $person_url;?>"><?php echo $person_url; ?></a></p>
-		<?php endif; ?>
-		<?php if( have_rows( 'uds_profile_social_media_icons' ) ): ?>
-			<ul class="person-social-medias">
-			<?php while( have_rows( 'uds_profile_social_media_icons' ) ): the_row(); ?>
-				<?php
-					/**
-					 * See if the chosen value is in our base array of social media sites.
-					 * If not, use the hand-entered values
-					 */
-					$current_social_network = get_sub_field( 'uds_profile_network_name' );
-					if( array_key_exists( $current_social_network, $base_social_media ) ) {
-						$network_name = $base_social_media[$current_social_network]['name'];
-						$network_icon = $base_social_media[$current_social_network]['icon'];
-					}else{
-						$network_name = get_sub_field( 'uds_profile_custom_network_name');
-						$network_icon = get_sub_field( 'uds_profile_social_icon' );
-					}
-				?>
-				<li>
-					<a
-					aria-label="Go to user <?php $network_name; ?> profile"
-					href="<?php echo get_sub_field('uds_profile_social_url')?>"
-					>
-						<span class="<?php echo $network_icon; ?>" />
-					</a>
-				</li>
-			<?php endwhile; ?>
-			</ul>
-		<?php endif; ?>
+		</div>
 	</div>
 </div>
