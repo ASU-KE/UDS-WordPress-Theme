ff43fb81a1715a24ffcfbcc8b086ecc09c90ffcc
Walter McConnell <Walter.McConnell@asu.edu>
2021-11-30 17:10:55 -0700

    Enqueue JS Only When Button is On Page
    Following in the footsteps of our tabs, I moved the enqueuing of the JS for this block to the 'register.php' file, so it will only be loaded if there is a button on the page.

--- templates-blocks/show-more/register.php
+++ templates-blocks/show-more/register.php
@@ -16,6 +16,7 @@ acf_register_block_type(
 		'description'       => __( 'UDS show more button with configurable size, color, and icon.', 'uds-wordpress-theme' ), // description the user will see.
 		'icon'              => 'editor-insertmore', // Dashicon, or custom SVG code, for the icon.
 		'render_template'   => 'templates-blocks/show-more/show-more.php', // location of the block's template.
+		'enqueue_script'    => get_template_directory_uri() . '/js/show-more-btn.js', // Load javascript
 		'category'          => 'uds', // category this block appears in.
 		'keywords'          => array( 'button', 'show', 'more' ),
 		'supports'          => array(
