606b261f06762915dc71b8bd2fabf35c10b308db
Nathan Rollins <nathan.rollins@asu.edu>
2021-12-29 01:24:13 -0700

    remove old dist scripts in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -267,78 +267,6 @@ gulp.task("clean-vendor-assets", function () {
 	]);
 });
 
-/**
- * Deletes all files inside the dist folder and the folder itself.
- *
- * Run: gulp clean-dist
- */
-gulp.task("clean-dist", function () {
-	return del(paths.dist);
-});
-
-// Run
-// gulp dist
-// Copies the files to the dist folder for distribution as simple theme
-gulp.task(
-	"dist",
-	gulp.series(["clean-dist"], function () {
-		return gulp
-			.src(
-				[
-					"**/*",
-					"!" + paths.node,
-					"!" + paths.node + "/**",
-					"!" + paths.dev,
-					"!" + paths.dev + "/**",
-					"!" + paths.dist,
-					"!" + paths.dist + "/**",
-					"!" + paths.distprod,
-					"!" + paths.distprod + "/**",
-					"!" + paths.sass,
-					"!" + paths.sass + "/**",
-					"!" + paths.composer,
-					"!" + paths.composer + "/**",
-					"!+(readme|README).+(txt|md)",
-					"!*.+(dist|json|js|lock|xml)",
-					"!CHANGELOG.md",
-				],
-				{ buffer: true }
-			)
-			.pipe(gulp.dest(paths.dist));
-	})
-);
-
-/**
- * Deletes all files inside the dist-product folder and the folder itself.
- *
- * Run: gulp clean-dist-product
- */
-gulp.task("clean-dist-product", function () {
-	return del(paths.distprod);
-});
-
-// Run
-// gulp dist-product
-// Copies the files to the /dist-prod folder for distribution as theme with all assets
-gulp.task(
-	"dist-product",
-	gulp.series(["clean-dist-product"], function () {
-		return gulp
-			.src([
-				"**/*",
-				"!" + paths.node,
-				"!" + paths.node + "/**",
-				"!" + paths.composer,
-				"!" + paths.composer + "/**",
-				"!" + paths.dist,
-				"!" + paths.dist + "/**",
-				"!" + paths.distprod,
-				"!" + paths.distprod + "/**",
-			])
-			.pipe(gulp.dest(paths.distprod));
-	})
-);
-
 // Run
 // gulp reset-assets
 // Clear and re-copy the vendor assets
@@ -349,7 +277,7 @@ gulp.task("reset-assets", gulp.series("clean-vendor-assets", "copy-assets"));
 // Compiles the styles and scripts and runs the dist task
 gulp.task(
 	"compile",
-	gulp.series("styles", "scripts", "imagemin", "dist")
+	gulp.series("styles", "scripts", "imagemin")
 );
 
 // Run:
