7df3735dec1c36cd008fa6bd8870f199fc3b27a6
John Cox <john.k.cox@asu.edu>
2023-03-29 11:54:28 -0700

    fix end of file error

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -60,50 +60,8 @@ if (!function_exists('uds_wp_scripts')) {
 		$parent_org_name = get_theme_mod( 'parent_unit_name' );
 		$parent_org_link = get_theme_mod( 'parent_unit_link' );
 
-		// load current user status
-		global $current_user;
-/*
-		$menu_name   = 'primary';
-		$menu_items  = uds_react_get_menu_formatted_array( $menu_name );
-		echo '<script>console.log("enqueue animate title")</script>';
-		echo '<script>console.log('.json_encode($menu_items['animate-title']).')</script>';
-
-		// wp_die( var_dump( $menu_items ) );
-
-		// pass WordPress PHP variables to the uds-header-scripts script we enqueued above
-		// These variables are props for the header React component
-		wp_localize_script(
-			'uds-wordpress-scripts', // the handle of the script to pass our variables
-			'udsHeaderVars', // object name to access our PHP variables from in our script
-			// register an array of variables we would like to use in our script
-			array(
-				'loggedIn' => is_user_logged_in(),
-				'loginLink' => site_url() . '/wp-admin',
-				'logoutLink' => wp_logout_url(),
-				'userName' => $current_user->user_login,
-				'navTree' => $menu_items['nav-items'],
-				'mobileNavTree' => $menu_items['nav-items'], // define an alternate navigation menu for mobile view
-				'expandOnHover' => $menu_items['expand-on-hover'],
-				'baseUrl' => '/', // this could be very important for subfolder multisites where the menu base url (e.g. Home) must point to the current subsite, not the root.
-				'logo' => $menu_items['logo-override'],
-				// [
-				// 	'alt' => 'alt text',        // default: 'Arizona State University'
-				// 	'src' => '/wp-content/uploads/2022/11/US-Navy-logo.jpg',        // default: 'arizona-state-university-logo-vertical.png'
-				// 	'mobileSrc' => '/wp-content/uploads/2022/11/US-Navy-logo.jpg',  // default: 'arizona-state-university-logo.png'
-				// 	'brandLink' => 'https://asu.edu',  // default: 'https://asu.edu'
-				// ],
-				'isPartner' => $menu_items['show-partner-logo'],
-				'partnerLogo' => $menu_items['partner-logo'],
-				'title' => get_bloginfo(),
-				'animateTitle' => $menu_items['animate-title'],
-				'parentOrg' => $parent_org_name,
-				'parentOrgUrl' => $parent_org_link,
-				'breakpoint' => $menu_items['mobile-menu-breakpoint'],
-				'buttons' => $menu_items['cta-buttons'],
-			)
-		);
 	}
-} // End of if function_exists( 'uds_wp_scripts' ). */
+} // End of if function_exists( 'uds_wp_scripts' ).
 add_action( 'wp_enqueue_scripts', 'uds_wp_scripts' );
 
 
