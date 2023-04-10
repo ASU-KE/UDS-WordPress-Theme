1fce26520cf0f0cfd34f7dc607c1c36ff1509310
Walter McConnell <Walter.McConnell@asu.edu>
2023-04-10 11:37:05 -0700

    Add sidebar right page starters
    Added two page starters with a sidebar on the right,  but with an info box instead of a menu

---
+++ patterns/sidebar-right-page-with-breadcrumbs.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Title: Sidebar Right Page with breadcrumbs
+ * Slug: sidebar-right-page-with-breadcrumbs
+ * Description: An empty page layout with a sidebar on the right for a contact box or something similar and breadcrumbs above all content
+ * Categories: pages
+ * Keywords: sidebar, columns, two, breadcrumbs
+ * Viewport Width: 1200
+ * Block Types: core/post-content
+ * Post Types: page
+ * Inserter: true
+ */
+
+?>
+<!-- wp:unityblocks/hero {"mediaId":4863,"mediaUrl":"<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/patterns/images/medium-hero-placeholder.png","subTitleText":"Page starter","titleText":"Sidebar right with breadcrumbs","contentsText":"For when you want some kind of important content in that right-hand column but you also need to get back home.","hideContents":"yes"} -->
+<div id="unityblocks-hero" class="wp-block-unityblocks-hero hide-content" data-herotype="heading-hero" data-image="{&quot;url&quot;:&quot;<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/patterns/images/medium-hero-placeholder.png&quot;,&quot;altText&quot;:&quot;&quot;,&quot;cssClass&quot;:[],&quot;size&quot;:&quot;medium&quot;}" data-subtitle="{&quot;text&quot;:&quot;Page starter&quot;,&quot;highlightColor&quot;:&quot;white&quot;,&quot;maxWidth&quot;:&quot;&quot;,&quot;cssClass&quot;:[]}" data-title="{&quot;text&quot;:&quot;Sidebar right with breadcrumbs&quot;,&quot;highlightColor&quot;:&quot;gold&quot;,&quot;maxWidth&quot;:&quot;&quot;,&quot;cssClass&quot;:[]}" data-contents="[{&quot;text&quot;:&quot;For when you want some kind of important content in that right-hand column but you also need to get back home.&quot;,&quot;maxWidth&quot;:&quot;&quot;,&quot;cssClass&quot;:[],&quot;highlightColor&quot;:&quot;white&quot;}]" data-contentscolor="white"></div>
+<!-- /wp:unityblocks/hero -->
+
+<!-- wp:wp-bootstrap-blocks/container {"marginAfter":"mb-0"} -->
+<!-- wp:wp-bootstrap-blocks/row {"template":"custom","className":"mb-6 mb-xl-10"} -->
+<!-- wp:wp-bootstrap-blocks/column {"sizeXs":0} -->
+<!-- wp:yoast-seo/breadcrumbs {"className":"py-4 mb-0"} /-->
+
+<!-- wp:separator {"className":"my-0"} -->
+<hr class="wp-block-separator has-alpha-channel-opacity my-0"/>
+<!-- /wp:separator -->
+<!-- /wp:wp-bootstrap-blocks/column -->
+<!-- /wp:wp-bootstrap-blocks/row -->
+
+<!-- wp:wp-bootstrap-blocks/row {"template":"2-1","className":"justify-content-between"} -->
+<!-- wp:wp-bootstrap-blocks/column {"sizeMd":7,"className":"order-2 order-md-1"} -->
+<!-- wp:heading {"className":"mt-0"} -->
+<h2 class="wp-block-heading mt-0">Content column</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"className":"lead"} -->
+<p class="lead">This page has a Yoast breadcrumbs block at the top, which requires the Yoast plugin.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+<!-- /wp:paragraph -->
+<!-- /wp:wp-bootstrap-blocks/column -->
+
+<!-- wp:wp-bootstrap-blocks/column {"sizeMd":4,"className":"order-1 order-md-2 mb-6 mb-md-0"} -->
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|uds-size-2","right":"var:preset|spacing|uds-size-2","bottom":"var:preset|spacing|uds-size-2","left":"var:preset|spacing|uds-size-2"},"blockGap":"var:preset|spacing|uds-size-3"}},"backgroundColor":"white","textColor":"gray-7","className":"border","layout":{"type":"constrained"}} -->
+<div class="wp-block-group border has-gray-7-color has-white-background-color has-text-color has-background" style="padding-top:var(--wp--preset--spacing--uds-size-2);padding-right:var(--wp--preset--spacing--uds-size-2);padding-bottom:var(--wp--preset--spacing--uds-size-2);padding-left:var(--wp--preset--spacing--uds-size-2)"><!-- wp:heading -->
+<h2 class="wp-block-heading">Important box</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>We often use a right-hand sidebar for things like an important message or a contact card. This template is set up so that this box will show up on top of the page at mobile breakpoints.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:acf/uds-button {"name":"acf/uds-button","data":{"tag_type":"link","_tag_type":"field_6269c6c11bb7d","button_link":"","_button_link":"field_60522a8361f39","external_link":"0","_external_link":"field_601e39a0afff2","button_color":"maroon","_button_color":"field_601f031d80f7e","button_size":"default","_button_size":"field_601f61b7ff690","icon":"","_icon":"field_602c30da61fbb","remove_outer_div":"0","_remove_outer_div":"field_6269bd7ae75f4","button_attributes":"","_button_attributes":"field_626b104524e2a"},"mode":"preview"} /--></div>
+<!-- /wp:group -->
+<!-- /wp:wp-bootstrap-blocks/column -->
+<!-- /wp:wp-bootstrap-blocks/row -->
+<!-- /wp:wp-bootstrap-blocks/container -->
