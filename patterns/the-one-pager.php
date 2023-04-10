7b121dd1a7a09dc8e9905c92bb185a5092e5dbe9
Walter McConnell <Walter.McConnell@asu.edu>
2023-04-10 11:40:22 -0700

    Add One-pager starter
    Added a  starter pattern for a one-page site with an anchor menu at the top

---
+++ patterns/the-one-pager.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * Title: The One Pager
+ * Slug: the-one-pager
+ * Description: A basic hero with a standard container below it
+ * Categories: pages
+ * Keywords: page, container, full-width, anchor
+ * Viewport Width: 1200
+ * Block Types: core/post-content
+ * Post Types: page
+ * Inserter: true
+ */
+
+?>
+<!-- wp:unityblocks/hero {"mediaId":4863,"mediaUrl":"<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/patterns/images/medium-hero-placeholder.png","subTitleText":"Page starter","titleText":"The one-pager","contentsText":"A page with multiple sections and an anchor menu","hideContents":"yes"} -->
+<div id="unityblocks-hero" class="wp-block-unityblocks-hero hide-content" data-herotype="heading-hero" data-image="{&quot;url&quot;:&quot;<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/patterns/images/medium-hero-placeholder.png&quot;,&quot;altText&quot;:&quot;&quot;,&quot;cssClass&quot;:[],&quot;size&quot;:&quot;medium&quot;}" data-subtitle="{&quot;text&quot;:&quot;Page starter&quot;,&quot;highlightColor&quot;:&quot;white&quot;,&quot;maxWidth&quot;:&quot;&quot;,&quot;cssClass&quot;:[]}" data-title="{&quot;text&quot;:&quot;The one-pager&quot;,&quot;highlightColor&quot;:&quot;gold&quot;,&quot;maxWidth&quot;:&quot;&quot;,&quot;cssClass&quot;:[]}" data-contents="[{&quot;text&quot;:&quot;A page with multiple sections and an anchor menu&quot;,&quot;maxWidth&quot;:&quot;&quot;,&quot;cssClass&quot;:[],&quot;highlightColor&quot;:&quot;white&quot;}]" data-contentscolor="white"></div>
+<!-- /wp:unityblocks/hero -->
+
+<!-- wp:wp-bootstrap-blocks/container {"marginAfter":"mb-0"} -->
+<!-- wp:wp-bootstrap-blocks/row {"template":"custom"} -->
+<!-- wp:wp-bootstrap-blocks/column {"sizeXs":0} -->
+<!-- wp:unityblocks/anchor-menu {"itemTexts":["Section one","Section two","Section three","Section four"],"itemTargets":["section-one","section-two","section-three","section-four"]} -->
+<div id="unityblocks-anchor-menu" class="wp-block-unityblocks-anchor-menu" data-items="[{&quot;text&quot;:&quot;Section one&quot;,&quot;targetIdName&quot;:&quot;section-one&quot;},{&quot;text&quot;:&quot;Section two&quot;,&quot;targetIdName&quot;:&quot;section-two&quot;},{&quot;text&quot;:&quot;Section three&quot;,&quot;targetIdName&quot;:&quot;section-three&quot;},{&quot;text&quot;:&quot;Section four&quot;,&quot;targetIdName&quot;:&quot;section-four&quot;}]" data-firstelementid="" data-focusfirstfocusableelement="false"></div>
+<!-- /wp:unityblocks/anchor-menu -->
+<!-- /wp:wp-bootstrap-blocks/column -->
+<!-- /wp:wp-bootstrap-blocks/row -->
+
+<!-- wp:wp-bootstrap-blocks/row {"template":"custom"} -->
+<!-- wp:wp-bootstrap-blocks/column {"sizeXs":0} -->
+<!-- wp:heading -->
+<h2 class="wp-block-heading" id="section-one">Section one </h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"className":"lead"} -->
+<p class="lead">This is a paragraph that has been given the "lead" class for when you want to emphasize something.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:heading -->
+<h2 class="wp-block-heading" id="section-two">Section two </h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"className":"lead"} -->
+<p class="lead">This is a paragraph that has been given the "lead" class for when you want to emphasize something.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>This layout is <strong>not</strong> optimal for text, as running text across the full content area like this violates the design standard calling for no more than 72 characters per line.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:heading -->
+<h2 class="wp-block-heading" id="section-three">Section three </h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"className":"lead"} -->
+<p class="lead">This is a paragraph that has been given the "lead" class for when you want to emphasize something.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>This layout is <strong>not</strong> optimal for text, as running text across the full content area like this violates the design standard calling for no more than 72 characters per line.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:heading -->
+<h2 class="wp-block-heading" id="section-four">Section four </h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"className":"lead"} -->
+<p class="lead">This is a paragraph that has been given the "lead" class for when you want to emphasize something.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>This layout is <strong>not</strong> optimal for text, as running text across the full content area like this violates the design standard calling for no more than 72 characters per line.</p>
+<!-- /wp:paragraph -->
+<!-- /wp:wp-bootstrap-blocks/column -->
+<!-- /wp:wp-bootstrap-blocks/row -->
+<!-- /wp:wp-bootstrap-blocks/container -->
