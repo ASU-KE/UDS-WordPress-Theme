322f9be78da430121a2779f9a2446e3dd2e26860
Walter McConnell <Walter.McConnell@asu.edu>
2023-04-19 16:15:51 -0700

    Update Category Name
    Changed the "layouts" name to "layout containers".

--- inc/block-pattern-categories.php
+++ inc/block-pattern-categories.php
@@ -15,7 +15,7 @@ function uds_wordpress_register_pattern_categories() {
 		array( 'slug' => 'cards', 'label' => 'Cards'),
 		array( 'slug' => 'content-rows', 'label' => 'Content rows'),
 		array( 'slug' => 'image-and-text', 'label' => 'Image and text'),
-		array( 'slug' => 'layouts', 'label' => 'Layouts' ),
+		array( 'slug' => 'layout-containers', 'label' => 'Layout Containers' ),
 		array( 'slug' => 'dynamic', 'label' => 'News and Events'),
 		array( 'slug' => 'page-starters', 'label' => 'Page Starters'),
 		array( 'slug' => 'quotes', 'label' => 'Quotes' )
