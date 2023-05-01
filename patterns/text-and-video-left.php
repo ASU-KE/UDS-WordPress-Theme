ccc04d18c9a453ff9af2618f55b661954e212a0e
Walter McConnell <Walter.McConnell@asu.edu>
2023-05-01 11:30:26 -0700

    Add text and video patterns
    Added a pair of basic, two-column patterns for text and video next to each other.

---
+++ patterns/text-and-video-left.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Title: Text and Video Left
+ * Slug: text-and-video-left
+ * Description: A two column layout with text on the left and a YouTube embed block on the right.
+ * Categories: content-rows
+ * Keywords: column, two, right, text, video
+ * Viewport Width: 1200
+ * Block Types: 
+ * Post Types: 
+ * Inserter: true
+ */
+
+?>
+<!-- wp:wp-bootstrap-blocks/row {"template":"custom"} -->
+<!-- wp:wp-bootstrap-blocks/column {"sizeMd":6} -->
+<!-- wp:embed {"url":"https://youtu.be/dQw4w9WgXcQ","type":"video","providerNameSlug":"youtube","responsive":true,"className":"wp-embed-aspect-16-9 wp-has-aspect-ratio"} -->
+<figure class="wp-block-embed is-type-video is-provider-youtube wp-block-embed-youtube wp-embed-aspect-16-9 wp-has-aspect-ratio"><div class="wp-block-embed__wrapper">
+https://youtu.be/dQw4w9WgXcQ
+</div></figure>
+<!-- /wp:embed -->
+<!-- /wp:wp-bootstrap-blocks/column -->
+
+<!-- wp:wp-bootstrap-blocks/column {"sizeMd":6,"className":"mb-4 mb-xl-0"} -->
+<!-- wp:heading {"className":"mt-0"} -->
+<h2 class="wp-block-heading mt-0">Lorem ipsum</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+<!-- /wp:paragraph -->
+
+<!-- wp:acf/uds-button {"name":"acf/uds-button","data":{"tag_type":"link","_tag_type":"field_6269c6c11bb7d","button_link":"","_button_link":"field_60522a8361f39","external_link":"0","_external_link":"field_601e39a0afff2","button_color":"maroon","_button_color":"field_601f031d80f7e","button_size":"default","_button_size":"field_601f61b7ff690","icon":"","_icon":"field_602c30da61fbb","remove_outer_div":"0","_remove_outer_div":"field_6269bd7ae75f4","button_attributes":"","_button_attributes":"field_626b104524e2a"},"mode":"preview"} /-->
+<!-- /wp:wp-bootstrap-blocks/column -->
+<!-- /wp:wp-bootstrap-blocks/row -->
