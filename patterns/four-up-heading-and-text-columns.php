6c023559cf6a91c2583350d23101e7e8678d27e3
Walter McConnell <Walter.McConnell@asu.edu>
2023-05-01 11:31:18 -0700

    Add heading and text patters
    Added three patterns for quickly laying out a heading, a paragraph, and a link in two, three, or four columns

---
+++ patterns/four-up-heading-and-text-columns.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * Title: Four-up Heading and Text Columns
+ * Slug: four-up-heading-and-text-columns
+ * Description: A container with heading and four-column text for navigation
+ * Categories: text
+ * Keywords: four, text
+ * Viewport Width: 1200
+ * Block Types: 
+ * Post Types: 
+ * Inserter: true
+ */
+
+?>
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|uds-size-6","bottom":"var:preset|spacing|uds-size-6"}}},"layout":{"type":"default"}} -->
+<div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--uds-size-6);padding-bottom:var(--wp--preset--spacing--uds-size-6)"><!-- wp:wp-bootstrap-blocks/row {"template":"custom","className":"mb-2 mb-xl-4"} -->
+<!-- wp:wp-bootstrap-blocks/column -->
+<!-- wp:heading -->
+<h2 class="wp-block-heading">Lorem ipsum</h2>
+<!-- /wp:heading -->
+<!-- /wp:wp-bootstrap-blocks/column -->
+<!-- /wp:wp-bootstrap-blocks/row -->
+
+<!-- wp:wp-bootstrap-blocks/row {"template":"custom"} -->
+<!-- wp:wp-bootstrap-blocks/column {"sizeLg":3,"sizeMd":6,"className":"mb-4"} -->
+<!-- wp:heading {"level":3} -->
+<h3 class="wp-block-heading">Lorem ipsum</h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sit amet massa at nisi auctor molestie. Morbi cursus sapien vel odio scelerisque pretium.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p><a href="#">lorem ipsum</a></p>
+<!-- /wp:paragraph -->
+<!-- /wp:wp-bootstrap-blocks/column -->
+
+<!-- wp:wp-bootstrap-blocks/column {"sizeLg":3,"sizeMd":6,"className":"mb-4"} -->
+<!-- wp:heading {"level":3} -->
+<h3 class="wp-block-heading">Lorem ipsum</h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sit amet massa at nisi auctor molestie. Morbi cursus sapien vel odio scelerisque pretium.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p><a href="#">lorem ipsum</a></p>
+<!-- /wp:paragraph -->
+<!-- /wp:wp-bootstrap-blocks/column -->
+
+<!-- wp:wp-bootstrap-blocks/column {"sizeLg":3,"sizeMd":6,"className":"mb-4"} -->
+<!-- wp:heading {"level":3} -->
+<h3 class="wp-block-heading">Lorem ipsum</h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sit amet massa at nisi auctor molestie. Morbi cursus sapien vel odio scelerisque pretium.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p><a href="#">lorem ipsum</a></p>
+<!-- /wp:paragraph -->
+<!-- /wp:wp-bootstrap-blocks/column -->
+
+<!-- wp:wp-bootstrap-blocks/column {"sizeLg":3,"sizeMd":6,"className":"mb-4"} -->
+<!-- wp:heading {"level":3} -->
+<h3 class="wp-block-heading">Lorem ipsum</h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sit amet massa at nisi auctor molestie. Morbi cursus sapien vel odio scelerisque pretium.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p><a href="#">lorem ipsum</a></p>
+<!-- /wp:paragraph -->
+<!-- /wp:wp-bootstrap-blocks/column -->
+<!-- /wp:wp-bootstrap-blocks/row --></div>
+<!-- /wp:group -->
