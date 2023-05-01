4f53b9b7a432d712400efa3df530e9755e4b53d2
Walter McConnell <Walter.McConnell@asu.edu>
2023-05-01 11:43:31 -0700

    Add/Update Nested Patterned Containers
    Add two new patterns for content containers nested within full-width pattered backgrounds; edited on other.

---
+++ patterns/nested-with-pattern-network-dark.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Title: Nested with pattern (network dark)
+ * Slug: nested-with-pattern-network-dark
+ * Description: A full-width Bootstrap container with a pattern background and content-width container inside
+ * Categories: layout-containers
+ * Keywords: pattern, container, full, background, dark
+ * Viewport Width: 1920
+ * Block Types: 
+ * Post Types: 
+ * Inserter: true
+ */
+
+?>
+<!-- wp:wp-bootstrap-blocks/container {"isFluid":true,"marginAfter":"mb-0","className":"py-6 py-xl-12 bg network-black"} -->
+<!-- wp:wp-bootstrap-blocks/container {"marginAfter":"mb-0"} -->
+<!-- wp:wp-bootstrap-blocks/row {"template":"custom"} -->
+<!-- wp:wp-bootstrap-blocks/column {"sizeXs":0} /-->
+<!-- /wp:wp-bootstrap-blocks/row -->
+<!-- /wp:wp-bootstrap-blocks/container -->
+<!-- /wp:wp-bootstrap-blocks/container -->
