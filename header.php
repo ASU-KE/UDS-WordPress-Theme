bd47d0852bc78bdeabb3409300dce430e77b979e
John Cox <john.k.cox@asu.edu>
2024-07-25 16:15:15 -0700

    mv gtm functions to head/body

--- header.php
+++ header.php
@@ -52,6 +52,13 @@ if (!empty($c_options['hotjar_site_id'])) {
 <html <?php language_attributes(); ?>>
 
 <head>
+	<!-- Google Tag Manager -->
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+	})(window,document,'script','dataLayer','GTM-KDWN8Z');</script>
+	<!-- End Google Tag Manager -->
 	<meta charset="<?php bloginfo('charset'); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
@@ -67,11 +74,6 @@ if (!empty($c_options['hotjar_site_id'])) {
 			}
 		</style>
 	<?php
-	}
-
-	// ASU Hub Analytics.
-	if (!empty($asu_hub_analytics) && 'enabled' === $asu_hub_analytics) {
-		include get_template_directory() . '/inc/analytics/asu-gtm-analytics-tracking-code.php';
 	}
 
 	// Lavidge Adwords tracking code.
@@ -124,7 +126,10 @@ if (!empty($c_options['hotjar_site_id'])) {
 </head>
 
 <body <?php body_class(); ?> <?php uds_wp_body_attributes(); ?> id="back_to_top">
-
+	<!-- Google Tag Manager (noscript) -->
+	<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KDWN8Z"
+	height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+	<!-- End Google Tag Manager (noscript) -->
 	<?php
 	do_action('wp_body_open');
 
