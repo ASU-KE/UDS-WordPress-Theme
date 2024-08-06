92b15f5ca1a44560d91c46cf770c533b552063b7
John Cox <john.k.cox@asu.edu>
2024-08-06 11:46:09 -0700

    gulp task side menu active

--- src/js/theme.js
+++ src/js/theme.js
@@ -507,7 +507,11 @@ document.addEventListener('DOMContentLoaded', function () {
     let active = navbar.getElementsByClassName("is-active")[0];
     let parentClass = active.parentElement;
     if (parentClass.classList.contains("card-body")) {
-      parentClass.parentElement.classList.add("show");
+      if (parentClass.classList.contains("collapse")) {
+        parentClass.classList.add("show");
+      } else {
+        parentClass.parentElement.classList.add("show");
+      }
     }
   }
 });
