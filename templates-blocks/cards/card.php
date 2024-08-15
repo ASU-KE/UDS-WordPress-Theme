f74567d86055c56725d7744a6d204a238b0c7250
John Cox <john.k.cox@asu.edu>
2024-08-14 17:04:55 -0700

    add interactive card option

--- templates-blocks/cards/card.php
+++ templates-blocks/cards/card.php
@@ -97,6 +97,22 @@ if (!empty($image_data)) {
 <div class="uds-card-arrangement">
 	<div class="uds-card-arrangement-card-container">
 <?php endif; ?>
+<?php if ('interactive' == $style) : ?>
+	<div class="content-section text-white">
+		<div class="image-holder">
+			<img src="<?php $background_image = the_field('interactive_background_image');?>" alt="alt text" loading="lazy" decoding="async">
+		</div>
+		<div class="content-holder">
+			<div class="content-bg">
+				<h3><?php the_field('title'); ?></h3>
+				<div class="hidden-details">
+					<p class="long-text"><?php the_field('body_text', false, false); ?></p>
+					<a href="<?php $call_to_action_url = get_field('interactive_cta_url'); echo $call_to_action_url; ?>" class="btn btn-gold btn-sm" data-ga="call to action" data-ga-name="onclick" data-ga-event="link" data-ga-action="click" data-ga-type="<?php echo $data_ga_type = (str_contains($call_to_action_url, 'asu.edu') !== false || str_contains(substr($call_to_action_url, 0, 1), '/') !== false) ? 'internal link' : 'external link'; ?>" data-ga-region="main content" data-ga-section="<?php echo $heading; ?>"><?php $call_to_action = the_field('interactive_cta_button_text');  echo $call_to_action; ?></a>
+				</div>
+			</div>
+		</div>
+	</div>
+	<?php else : ?>
 <div class="card <?php echo $style_class; ?> <?php echo $orientation_class; ?> <?php echo $additional_classes; ?>">
 	<?php if ('image' == $header_style) : ?>
 		<img class="card-img-top" src="<?php echo $image_url; ?>" alt="<?php echo $image_alt; ?>">
@@ -243,7 +259,9 @@ if (!empty($image_data)) {
 	<?php endif; ?>
 	<?php if ('card-horizontal' == $orientation_class) : ?>
 </div> <!-- close horizontal content -->
+<?php endif; ?>
 	</div> <!-- close card arrangement card container -->
 </div> <!-- close card arrangement -->
 <?php endif; ?>
+
 </div>
