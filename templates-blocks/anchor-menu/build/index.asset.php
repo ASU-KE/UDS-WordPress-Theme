fac903289039a2588cecce98134f12c1b5f9554b
John Cox <john.k.cox@asu.edu>
2024-11-08 14:17:51 -0700

    build v1.0 mvp

--- templates-blocks/anchor-menu/build/index.asset.php
+++ templates-blocks/anchor-menu/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '7b0ef723e0a3b3c8d2b9');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '5a4ff40c5bdd1663e4bd');
--- templates-blocks/anchor-menu/build/view.asset.php
+++ templates-blocks/anchor-menu/build/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => '4f59a93f3ee137ec0776');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime'), 'version' => 'b23c946c7e200575f253');
