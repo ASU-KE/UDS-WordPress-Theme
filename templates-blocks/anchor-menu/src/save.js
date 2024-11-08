4765f0624188b105f23cd6a51b63e0bb7d3cadeb
John Cox <john.k.cox@asu.edu>
2024-11-08 14:16:41 -0700

    cp from unityblocks save

---
+++ templates-blocks/anchor-menu/src/save.js
@@ -0,0 +1,48 @@
+/**
+ * React hook that is used to mark the block wrapper element.
+ * It provides all the necessary props like the class name.
+ *
+ * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-block-editor/#useblockprops
+ */
+import { useBlockProps } from "@wordpress/block-editor";
+
+/**
+ * The save function defines the way in which the different attributes should
+ * be combined into the final markup, which is then serialized by the block
+ * editor into `post_content`.
+ *
+ * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-edit-save/#save
+ *
+ * @return {WPElement} Element to render.
+ */
+const save = (props) => {
+  const {
+    attributes: {
+      firstElementId,
+      focusFirstFocusableElement,
+      itemIcons,
+      itemTexts,
+      itemTargets,
+    },
+  } = props;
+
+  const items = itemTexts.map((itemText, index) => {
+    return {
+      icon: itemIcons[index],
+      text: itemText,
+      targetIdName: itemTargets[index],
+    };
+  });
+
+  return (
+    <div
+      id="unityblocks-anchor-menu-v2"
+      {...useBlockProps.save()}
+      data-items={JSON.stringify(items)}
+      data-firstElementId={firstElementId}
+      data-focusFirstFocusableElement={focusFirstFocusableElement}
+    ></div>
+  );
+};
+
+export default save;
