7183b0023032d65b4104874f3b9cbcb35ffd1fc5
John Cox <john.k.cox@asu.edu>
2024-11-14 14:16:51 -0700

    createRoot, import init, checkElement, WrapperComponent

--- templates-blocks/anchor-menu/src/view.js
+++ templates-blocks/anchor-menu/src/view.js
@@ -1,19 +1,38 @@
-const { render } = wp.element;
+import { useEffect } from "react"
+import { createRoot } from 'react-dom/client'
 
-import { AnchorMenu } from "../node_modules/@asu/components-core/dist/libCore.es";
+import { AnchorMenu } from "../node_modules/@asu/components-core/dist/libCore.es"
+import { initializeAnchorMenu } from "./initAnchorMenu"
 
-// Load first element with the unityblocks-anchormenu id
-const menu = document.querySelector("#unityblocks-anchor-menu-v2");
+const menu = document.querySelector("#unityblocks-anchor-menu-v2")
+const root = createRoot(menu);
 
 const items = JSON.parse(menu.dataset.items);
 const firstElementId = menu.dataset.firstElementId;
 const focusFirstFocusableElement = menu.dataset.focusFirstFocusableElement;
 
-render(
-  <AnchorMenu
-    items={items}
-    firstElementId={firstElementId}
-    focusFirstFocusableElement={focusFirstFocusableElement}
-  />,
-  menu
-);
+const checkElement = async selector => {
+  while ( document.querySelector(selector) === null) {
+    await new Promise( resolve =>  requestAnimationFrame(resolve) )
+  }
+  return document.querySelector(selector);
+};
+
+const WrapperComponent = () => {
+	useEffect(() => {
+		checkElement('#asuHeader').then((selector) => {
+			console.log(`check element ran: ${selector}`);
+		initializeAnchorMenu();
+		});
+	}, []);
+	const props = {
+		items: items,
+		firstElementId: firstElementId,
+		focusFirstFocusableElement: focusFirstFocusableElement,
+
+	}
+  return <AnchorMenu {...props} />;
+};
+
+export default WrapperComponent;
+root.render(<WrapperComponent/>)
