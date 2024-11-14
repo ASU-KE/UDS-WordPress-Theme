03bb6e79d28c6d37b8040086401d5c5708721ea0
John Cox <john.k.cox@asu.edu>
2024-11-14 14:18:43 -0700

    build latest

--- templates-blocks/anchor-menu/build/index.asset.php
+++ templates-blocks/anchor-menu/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '5a4ff40c5bdd1663e4bd');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '1c2785e0e1511daf4ea6');
--- templates-blocks/anchor-menu/build/view.asset.php
+++ templates-blocks/anchor-menu/build/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime'), 'version' => 'b23c946c7e200575f253');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime'), 'version' => '51f394c9f53ae1eecbc3');
