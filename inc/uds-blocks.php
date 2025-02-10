38571f7aa2e56a1252ae74deca577dc9d0f83dc5
John Cox <john.k.cox@asu.edu>
2025-02-10 15:17:24 -0700

    register acf block hero-video

--- inc/uds-blocks.php
+++ inc/uds-blocks.php
@@ -135,4 +135,5 @@ if ( ! function_exists( 'remove_core_patterns' ) ) {
 add_action( 'init', 'register_acf_blocks', 5 );
 function register_acf_blocks() {
 	register_block_type( get_template_directory() . '/templates-blocks/cards' );
+	register_block_type( get_template_directory() . '/templates-blocks/hero-video' );
 }
