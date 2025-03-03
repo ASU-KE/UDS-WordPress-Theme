5291056392efbe16dd0a49191ca15dbe98ae2a31
John Cox <john.k.cox@asu.edu>
2025-03-03 12:59:13 -0700

    del legacy init

--- src/js/custom/init-cookie-consent.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("DOMContentLoaded", event => {
-	AsuCookieConsent.initCookieConsent({
-		targetSelector: "#cookieConsentContainer",
-		props: {
-			enableCookieConsent: true,
-			expirationTime: 90, // Number of days to expire the consent
-		},
-	});
-});
