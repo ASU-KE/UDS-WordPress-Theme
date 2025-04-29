1103fed6850778b33ffc1de13d41f7ad7645cf34
John Cox <john.k.cox@asu.edu>
2025-04-29 16:30:30 -0700

    fix vertical and feature classes, set default img

--- templates-blocks/profile/profile.php
+++ templates-blocks/profile/profile.php
@@ -81,10 +81,10 @@ if ( empty( $person_email ) && empty( $person_phone ) && empty( $person_street_a
 
 // Render the block.
 ?>
-<div class="uds-person-profile <?php echo $feature_class; ?>">
+<div class="uds-person-profile <?php echo $vertical_classes; ?>">
 	<div class="profile-img-container">
 		<div class="profile-img-placeholder">
-			<img class="profile-img" src="<?php echo $image_data['url']; ?>" alt="<?php echo $person_name; ?>" width="300" height="300" decoding="async" loading="lazy" fetchpriority="high">
+			<img class="profile-img <?php echo $feature_class; ?>" src="<?php echo $image_data['url'] ?? '/wp-content/themes/UDS-WordPress-Theme/dist/img/admin/headshot-placeholder.png'; ?>" alt="<?php echo $person_name; ?>" width="300" height="300" decoding="async" loading="lazy">
 		</div>
 	</div>
 	<div class="person">
