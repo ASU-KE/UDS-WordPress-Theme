c60f737ee4bcc336c8739d85af954f8040cefad8
John Cox <john.k.cox@asu.edu>
2025-10-14 11:04:20 -0700

    include data layer in front end scripts

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -99,6 +99,7 @@ gulp.task("front-end-scripts", function() {
 		"./src/js/fontawesome/solid.js",
 		"./src/js/custom/skip-link-focus-fix.js",
 		"./src/js/custom/init-uds-header.js",
+		"./src/js/uds/data-layer.js",
 		"./src/js/custom/init-data-layer.js",
 		"./src/js/custom/hero_video.js",
 		"./src/js/custom/modals.js",
