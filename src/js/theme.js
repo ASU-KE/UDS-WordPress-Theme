d9aa5d85a9bedfc72e609b90f048dceea250c8db
John Cox <john.k.cox@asu.edu>
2025-10-20 10:18:08 -0700

    a11y latest accordion

--- src/js/theme.js
+++ src/js/theme.js
@@ -4003,7 +4003,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
   $(document).ready(function () {
     // Add keydown event listener to accordion headers
-    $(document).on('keydown', '.accordion-header h3 a[role="button"]', function (e) {
+    $(document).on('keydown', '.accordion-header h3 a[role="button"], .accordion-header h4 a[role="button"]', function (e) {
       // Check if Space bar (keyCode 32) was pressed
       if (e.which === 32 || e.keyCode === 32) {
         // Prevent default scrolling behavior of Space bar
