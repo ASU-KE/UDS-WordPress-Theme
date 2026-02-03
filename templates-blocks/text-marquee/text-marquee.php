9f608856086a304136ee4bdf2954d19e5c665e4a
John Cox <john.k.cox@asu.edu>
2026-02-03 15:20:14 -0700

    update play pause btns

--- templates-blocks/text-marquee/text-marquee.php
+++ templates-blocks/text-marquee/text-marquee.php
@@ -56,12 +56,17 @@ if ( isset( $block['style']['color']['text'] ) ) {
 		<span class="marquee-text" aria-hidden="true"><?php echo wp_kses_post( $marquee_text ); ?></span>
 	</div>
 	
-	<button class="uds-marquee-pause-btn" 
-	        aria-label="Pause scrolling text"
-	        aria-pressed="false"
-	        type="button">
-		<span class="pause-icon" aria-hidden="true">⏸</span>
-		<span class="play-icon" aria-hidden="true" style="display: none;">▶</span>
-		<span class="sr-only">Pause</span>
-	</button>
+	<button id="playMarquee" type="button" class="btn btn-circle btn-circle-alt-white btn-circle-large uds-marquee-play-btn" data-ga="play text marquee" data-ga-name="onclick" data-ga-event="button" data-ga-action="click" data-ga-type="animation play" data-ga-region="text marquee" data-ga-section="text marquee">
+        <svg class="svg-inline--fa fa-play" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="play" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512" data-fa-i2svg="">
+            <path fill="currentColor" d="M73 39c-14.8-9.1-33.4-9.4-48.5-.9S0 62.6 0 80V432c0 17.4 9.4 33.4 24.5 41.9s33.7 8.1 48.5-.9L361 297c14.3-8.7 23-24.2 23-41s-8.7-32.2-23-41L73 39z"></path>
+        </svg>
+        <span class="visually-hidden">Play text marquee</span>
+    </button>
+    <button id="pauseMarquee" type="button" class="btn btn-circle btn-circle-alt-white btn-circle-large uds-marquee-pause-btn" data-ga="pause text marquee" data-ga-name="onclick" data-ga-event="button" data-ga-action="click" data-ga-type="animation pause" data-ga-region="text marquee" data-ga-section="text marquee">
+        <svg class="svg-inline--fa fa-pause" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="pause" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" data-fa-i2svg="">
+            <path fill="currentColor" d="M48 64C21.5 64 0 85.5 0 112V400c0 26.5 21.5 48 48 48H80c26.5 0 48-21.5 48-48V112c0-26.5-21.5-48-48-48H48zm192 0c-26.5 0-48 21.5-48 48V400c0 26.5 21.5 48 48 48h32c26.5 0 48-21.5 48-48V112c0-26.5-21.5-48-48-48H240z"></path>
+        </svg>
+        <span class="visually-hidden">Pause text marquee</span>
+    </button>
+	
 </div>
